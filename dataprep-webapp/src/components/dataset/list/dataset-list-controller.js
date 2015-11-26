b1b48d1315a02880668634b8443a95f63d9e3af1
cnguyen <cnguyen@talend.com>
2015-11-26 09:52:06 +0100

    TDP-401: fix problem "folder name is incorrect due to unicity check"

--- dataprep-webapp/src/components/dataset/list/dataset-list-controller.js
+++ dataprep-webapp/src/components/dataset/list/dataset-list-controller.js
@@ -15,10 +15,9 @@
      * @requires data-prep.services.uploadWorkflowService.service:UploadWorkflowService
      * @requires data-prep.services.datasetWorkflowService:UpdateWorkflowService
      * @requires data-prep.services.folder.service:FolderService
-     * @requires data-prep.services.folder.service:FolderRestService
      */
     function DatasetListCtrl($stateParams, StateService, DatasetService, DatasetListSortService, PlaygroundService,
-                             TalendConfirmService, MessageService, UploadWorkflowService, UpdateWorkflowService, FolderService, state, FolderRestService) {
+                             TalendConfirmService, MessageService, UploadWorkflowService, UpdateWorkflowService, FolderService, state) {
         var vm = this;
 
         vm.datasetService = DatasetService;
@@ -33,16 +32,6 @@
          * @type {String}
          */
         vm.folderName = '';
-
-
-        /**
-         * @ngdoc property
-         * @name folderExistAlreadyInCurrentFolder
-         * @propertyOf data-prep.dataset-list.controller:DatasetListCtrl
-         * @description The flag checking if the folder exist already in the current folder
-         * @type {Boolean}
-         */
-        vm.folderExistAlreadyInCurrentFolder = true;
 
         /**
          * @ngdoc property
@@ -244,32 +233,11 @@
 
         /**
          * @ngdoc method
-         * @name folderExistInCurrentFolder
-         * @methodOf  data-prep.dataset-list.controller:DatasetListCtrl
-         * @param {string} name The dataset name
-         * @description Check if folder that has the wanted name within the folder
-         * @returns {boolean} True if folder already, False if otherwise
-         */
-        vm.folderExistInCurrentFolder = function (name) {
-            if (name === '') {
-                vm.folderExistAlreadyInCurrentFolder = true;
-            } else {
-                FolderRestService.getFolderContent(state.folder.currentFolder).then(function(content) {
-                    vm.folderExistAlreadyInCurrentFolder = !!_.find(content.data.folders, function(folder){
-                        return folder.name.toLowerCase() === name.toLowerCase();
-                    });
-                });
-            }
-        };
-
-        /**
-         * @ngdoc method
          * @name actionsOnAddFolderClick
          * @methodOf data-prep.dataset-list.controller:DatasetListCtrl
          * @description run these action when clicking on Add Folder button
          */
         vm.actionsOnAddFolderClick = function(){
-            vm.folderExistAlreadyInCurrentFolder = true;
             vm.folderNameModal = true;
             vm.folderName = '';
         };
@@ -281,10 +249,14 @@
          * @description Create a new folder
          */
         vm.addFolder = function(){
+
+            vm.folderNameForm.$commitViewValue();
+
             var pathToCreate = (state.folder.currentFolder.id?state.folder.currentFolder.id:'') + '/' + vm.folderName;
             FolderService.create( pathToCreate )
                 .then(function() {
                     vm.goToFolder(state.folder.currentFolder);
+                    vm.folderNameModal = false;
                 });
         };
         /**

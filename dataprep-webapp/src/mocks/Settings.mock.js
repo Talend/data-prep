9b09b56dc32727fa47ca9469ce5d117579c544f5
jsomsanith <jsomsanith@talend.com>
2016-12-08 16:47:13 +0100

    feat: change dataset menu icon

--- dataprep-webapp/src/mocks/Settings.mock.js
+++ dataprep-webapp/src/mocks/Settings.mock.js
@@ -95,7 +95,7 @@ const settingsMock = {
 		'menu:datasets': {
 			id: 'menu:datasets',
 			name: 'Datasets',
-			icon: 'talend-cluster',
+			icon: 'talend-datastore',
 			type: '@@router/GO',
 			payload: {
 				method: 'go',

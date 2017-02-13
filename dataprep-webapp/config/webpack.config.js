998bff8f05750b751a17ec043dc2907e2accc83c
jsomsanith <jsomsanith@talend.com>
2017-02-13 11:55:04 +0100

    Remove eof and ttf files

--- dataprep-webapp/config/webpack.config.js
+++ dataprep-webapp/config/webpack.config.js
@@ -65,8 +65,6 @@ function getDefaultConfig(options) {
 					exclude: INDEX_TEMPLATE_PATH,
 				},
 				{ test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader', options: { name: '/assets/fonts/[name].[ext]', limit: 10000, mimetype: 'application/font-woff' } },
-				{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader', options: { name: '/assets/fonts/[name].[ext]', limit: 10000, mimetype: 'application/octet-stream' } },
-				{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader', options: { name: '/assets/fonts/[name].[ext]' } },
 				{ test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader', options: { name: '/assets/fonts/[name].[ext]', limit: 10000, mimetype: 'image/svg+xml' } },
 			]
 		},

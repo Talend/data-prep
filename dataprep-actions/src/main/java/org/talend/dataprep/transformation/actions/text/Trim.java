2e7c0638fb7c88a94082f18396e48e798231f4aa
msjian <msjian@talend.com>
2017-03-23 14:49:24 +0800

    TDQ-13269: improve some details

--- dataprep-actions/src/main/java/org/talend/dataprep/transformation/actions/text/Trim.java
+++ dataprep-actions/src/main/java/org/talend/dataprep/transformation/actions/text/Trim.java
@@ -51,12 +51,14 @@ public class Trim extends AbstractActionMetadata implements ColumnAction {
     public static final String CUSTOM_PADDING_CHAR_PARAMETER = "custom_padding_character"; //$NON-NLS-1$
 
     /** String Converter help class. */
-    public static final String STRING_CONVERTRT = "string_converter"; //$NON-NLS-1$
+    public static final String STRING_CONVERTER = "string_converter"; //$NON-NLS-1$
 
     /**
      * Keys used in the values of different parameters:
      */
     public static final String CUSTOM = "custom"; //$NON-NLS-1$
+
+    public static final String WHITESPACE = "whitespace"; //$NON-NLS-1$
 
     @Override
     public String getName() {
@@ -80,10 +82,10 @@ public class Trim extends AbstractActionMetadata implements ColumnAction {
         // @formatter:off
         parameters.add(SelectParameter.Builder.builder()
                 .name(PADDING_CHAR_PARAMETER)
-                .item("whitespace", "whitespace") //$NON-NLS-1$ //$NON-NLS-2$
-                .item(CUSTOM, CUSTOM,new Parameter(CUSTOM_PADDING_CHAR_PARAMETER, ParameterType.STRING, StringUtils.EMPTY)) 
+                .item(WHITESPACE)
+                .item(CUSTOM, CUSTOM, new Parameter(CUSTOM_PADDING_CHAR_PARAMETER, ParameterType.STRING, StringUtils.EMPTY)) 
                 .canBeBlank(true)
-                .defaultValue("whitespace") //$NON-NLS-1$
+                .defaultValue(WHITESPACE)
                 .build());
         // @formatter:on
         return ActionsBundle.attachToAction(parameters, this);
@@ -93,7 +95,7 @@ public class Trim extends AbstractActionMetadata implements ColumnAction {
     public void compile(ActionContext actionContext) {
         super.compile(actionContext);
         if (actionContext.getActionStatus() == ActionContext.ActionStatus.OK) {
-            actionContext.get(STRING_CONVERTRT, p -> new StringConverter());
+            actionContext.get(STRING_CONVERTER, p -> new StringConverter());
         }
     }
 
@@ -106,7 +108,7 @@ public class Trim extends AbstractActionMetadata implements ColumnAction {
         final String toTrim = row.get(columnId);
         if (toTrim != null) {
             final Map<String, String> parameters = context.getParameters();
-            final StringConverter stringConverter = context.get(STRING_CONVERTRT);
+            final StringConverter stringConverter = context.get(STRING_CONVERTER);
             if (CUSTOM.equals(parameters.get(PADDING_CHAR_PARAMETER))) {
                 row.set(columnId, stringConverter.removeTrailingAndLeading(toTrim, parameters.get(CUSTOM_PADDING_CHAR_PARAMETER)));
             } else {

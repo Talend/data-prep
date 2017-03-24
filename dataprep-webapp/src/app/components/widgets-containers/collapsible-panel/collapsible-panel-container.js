64033493b988427fa673c9edc0f35479398ecc3d
amaalej <amaalej@talend.com>
2017-03-24 18:17:37 +0100

    feat(TDP-3331): new collapsible component

--- dataprep-webapp/src/app/components/widgets-containers/collapsible-panel/collapsible-panel-container.js
+++ dataprep-webapp/src/app/components/widgets-containers/collapsible-panel/collapsible-panel-container.js
@@ -1,15 +1,15 @@
- /*  ============================================================================
+/*  ============================================================================
 
-  Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+ Copyright (C) 2006-2016 Talend Inc. - www.talend.com
 
-  This source code is available under agreement available at
-  https://github.com/Talend/data-prep/blob/master/LICENSE
+ This source code is available under agreement available at
+ https://github.com/Talend/data-prep/blob/master/LICENSE
 
-  You should have received a copy of the agreement
-  along with this program; if not, write to Talend SA
-  9 rue Pages 92150 Suresnes, France
+ You should have received a copy of the agreement
+ along with this program; if not, write to Talend SA
+ 9 rue Pages 92150 Suresnes, France
 
-  ============================================================================*/
+ ============================================================================*/
 
 import CollapsiblePanelCtrl from './collapsible-panel-controller';
 
@@ -17,7 +17,13 @@ const CollapsiblePanelContainer = {
 	template: `
 		<pure-collapsible-panel
 			header="$ctrl.header"
-			content="$ctrl.item.content">
+			content="$ctrl.item.content"
+			onSelect="$ctrl.item.onSelect"
+			onToggle="$ctrl.item.onToggle"
+			selected="$ctrl.item.selected"
+			expanded="$ctrl.item.expanded"
+			theme="$ctrl.item.theme"
+		>
 		</pure-collapsible-panel>
 	`,
 	bindings: {

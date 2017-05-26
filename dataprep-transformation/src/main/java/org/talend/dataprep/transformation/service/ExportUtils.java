14f2d0975a0a47a9ee47b174b3bd5b3b3b7b8c0b
skermabon <skermabon@talend.com>
2017-05-26 16:00:12 +0200

    bug(TDP-2925) Back - URL Encode the filename in full run result download

--- dataprep-transformation/src/main/java/org/talend/dataprep/transformation/service/ExportUtils.java
+++ dataprep-transformation/src/main/java/org/talend/dataprep/transformation/service/ExportUtils.java
@@ -1,14 +1,32 @@
 package org.talend.dataprep.transformation.service;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.UnsupportedEncodingException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.util.UriUtils;
 import org.talend.dataprep.format.export.ExportFormat;
 import org.talend.dataprep.http.HttpResponseContext;
 
 public class ExportUtils {
+
     private ExportUtils() {
     }
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExportUtils.class);
+
     public static void setExportHeaders(String exportName, ExportFormat format) {
         HttpResponseContext.contentType(format.getMimeType());
-        HttpResponseContext.header("Content-Disposition", "attachment; filename=\"" + exportName + format.getExtension() + "\"");
+        // TDP-2925 a multi-byte file name cannot export the file correctly
+        try {
+            HttpResponseContext.header("Content-Disposition",
+                    "attachment; filename=\"" + UriUtils.encodePath(exportName, UTF_8.toString()) + format.getExtension() + "\"");
+        } catch (UnsupportedEncodingException e) {
+            LOGGER.info("Can't encode '{}', will return the original name", exportName);
+            HttpResponseContext.header("Content-Disposition",
+                    "attachment; filename=\"" + exportName + format.getExtension() + "\"");
+        }
     }
 }

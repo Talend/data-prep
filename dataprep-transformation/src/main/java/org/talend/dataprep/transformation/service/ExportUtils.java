4d8f258a690eecc08bec572abdeed47a3f0e4488
skermabon <skermabon@talend.com>
2017-05-26 18:12:34 +0200

    bug(TDP-2925) Back - Feedback from Henri Mayeul

--- dataprep-transformation/src/main/java/org/talend/dataprep/transformation/service/ExportUtils.java
+++ dataprep-transformation/src/main/java/org/talend/dataprep/transformation/service/ExportUtils.java
@@ -6,6 +6,7 @@ import java.io.UnsupportedEncodingException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.util.UriUtils;
 import org.talend.dataprep.format.export.ExportFormat;
 import org.talend.dataprep.http.HttpResponseContext;
@@ -20,13 +21,13 @@ public class ExportUtils {
     public static void setExportHeaders(String exportName, ExportFormat format) {
         HttpResponseContext.contentType(format.getMimeType());
         // TDP-2925 a multi-byte file name cannot export the file correctly
+        // Current [RFC 2045] grammar restricts parameter values (and hence Content-Disposition filenames) to US-ASCII
         try {
-            HttpResponseContext.header("Content-Disposition",
+            HttpResponseContext.header(HttpHeaders.CONTENT_DISPOSITION,
                     "attachment; filename=\"" + UriUtils.encodePath(exportName, UTF_8.toString()) + format.getExtension() + "\"");
         } catch (UnsupportedEncodingException e) {
-            LOGGER.info("Can't encode '{}', will return the original name", exportName);
-            HttpResponseContext.header("Content-Disposition",
-                    "attachment; filename=\"" + exportName + format.getExtension() + "\"");
+            LOGGER.error("Can't encode '{}' from UTF-8", exportName);
+            throw new RuntimeException(e);
         }
     }
 }

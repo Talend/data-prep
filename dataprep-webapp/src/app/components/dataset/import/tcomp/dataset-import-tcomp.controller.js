b3802d34a86fd74b9ff09f428858f78834f9b996
Fabien Rassinier <frassinier@talend.com>
2017-06-01 13:30:24 +0200

    Update dataset-import-tcomp.controller.js

--- dataprep-webapp/src/app/components/dataset/import/tcomp/dataset-import-tcomp.controller.js
+++ dataprep-webapp/src/app/components/dataset/import/tcomp/dataset-import-tcomp.controller.js
@@ -92,8 +92,8 @@ export default class DatasetImportTcompCtrl {
 			.getDatasetForm(formData)
 			.then(({ data }) => {
 				this._getDatasetFormActions();
-					this.datasetForm = data;
-				});
+				this.datasetForm = data;
+			});
 	}
 
 	/**

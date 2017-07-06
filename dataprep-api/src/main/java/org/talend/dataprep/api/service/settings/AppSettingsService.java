86766d8179856687b1c9dc21d12b57eaf5853610
jbrun <jbrun@talend.com>
2017-07-06 15:28:14 +0200

    TDP-2573 formatting

--- dataprep-api/src/main/java/org/talend/dataprep/api/service/settings/AppSettingsService.java
+++ dataprep-api/src/main/java/org/talend/dataprep/api/service/settings/AppSettingsService.java
@@ -65,9 +65,7 @@ public class AppSettingsService {
 
         // populate appSettings actions dictionary (key: actionId, value: action)
         getSettingsStream(actionsProviders, actionsConfigurers) //
-                .filter(actionSettings ->
-                        actionSettings.getFiltered() == null
-                                || !actionSettings.getFiltered())
+                .filter(actionSettings -> actionSettings.getFiltered() == null || !actionSettings.getFiltered())
                 .forEach(action -> appSettings.getActions().put(action.getId(), action));
 
         // populate appSettings views dictionary (key: viewId, value: view)

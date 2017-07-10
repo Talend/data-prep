0dfd0d8522331f838037e0c8fba9e20e08643608
Henri-Mayeul de Benque <hmdebenque@talend.com>
2017-07-10 15:11:55 +0200

    feat(TDP-4136): use javax.validation to validate preparations

--- dataprep-preparation/src/main/java/org/talend/dataprep/preparation/service/PreparationService.java
+++ dataprep-preparation/src/main/java/org/talend/dataprep/preparation/service/PreparationService.java
@@ -143,8 +143,6 @@ public class PreparationService {
      * @return the created preparation id.
      */
     public String create(final Preparation preparation, String folderId) {
-        PreparationValidator.validateOrThrowException(preparation);
-
         LOGGER.debug("Create new preparation for data set {} in {}", preparation.getDataSetId(), folderId);
 
         Preparation toCreate = new Preparation(UUID.randomUUID().toString(), versionService.version().getVersionId());
@@ -443,7 +441,6 @@ public class PreparationService {
      * @return the updated preparation id.
      */
     public String update(String id, final Preparation preparation) {
-        PreparationValidator.validateOrThrowException(preparation);
         Preparation previousPreparation = preparationRepository.get(id, Preparation.class);
 
         // no preparation found

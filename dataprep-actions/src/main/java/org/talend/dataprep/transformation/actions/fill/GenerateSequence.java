2515b9d21565f8a4b68aa1f12b6513f141f6153a
qiongli <qiongli@talend.com>
2017-08-08 11:42:13 +0800

    fea(TDP-946 TDQ-13268 ): Create function 'Generate a sequence'

---
+++ dataprep-actions/src/main/java/org/talend/dataprep/transformation/actions/fill/GenerateSequence.java
@@ -0,0 +1,82 @@
+package org.talend.dataprep.transformation.actions.fill;
+
+import org.talend.dataprep.api.action.Action;
+import org.talend.dataprep.api.dataset.ColumnMetadata;
+import org.talend.dataprep.api.dataset.row.DataSetRow;
+import org.talend.dataprep.api.type.Type;
+import org.talend.dataprep.i18n.ActionsBundle;
+import org.talend.dataprep.parameters.Parameter;
+import org.talend.dataprep.parameters.ParameterType;
+import org.talend.dataprep.transformation.actions.category.ActionCategory;
+import org.talend.dataprep.transformation.actions.common.AbstractActionMetadata;
+import org.talend.dataprep.transformation.actions.common.ColumnAction;
+import org.talend.dataprep.transformation.api.action.context.ActionContext;
+import org.talend.dataquality.converters.DuplicateCharEraser;
+
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by qiongli on 8/3/2017.
+ */
+@Action(AbstractActionMetadata.ACTION_BEAN_PREFIX + GenerateSequence.ACTION_NAME)
+public class GenerateSequence extends AbstractActionMetadata implements ColumnAction {
+
+    public static final String ACTION_NAME = "generate_a_sequence";
+    /** The starting value of sequence.*/
+    protected static final String START_VALUE = "start_value";
+
+    /** The step value of sequence.*/
+    protected static final String STEP_VALUE = "step_value";
+
+    @Override
+    public String getName() {
+        return GenerateSequence.ACTION_NAME;
+    }
+
+    @Override
+    public List<Parameter> getParameters() {
+        final List<Parameter> parameters = super.getParameters();
+        Parameter startParameter = new Parameter(START_VALUE, ParameterType.INTEGER, "1");
+        parameters.add(startParameter);
+        Parameter stepParameter = new Parameter(STEP_VALUE, ParameterType.INTEGER, "1");
+        parameters.add(stepParameter);
+        return ActionsBundle.attachToAction(parameters, this);
+    }
+
+    @Override
+    public String getCategory() {
+        return ActionCategory.NUMBERS.getDisplayName();
+    }
+
+    @Override
+    public boolean acceptField(ColumnMetadata column) {
+        return  true;
+    }
+
+    @Override
+    public Set<Behavior> getBehavior() {
+            return EnumSet.of(Behavior.VALUES_COLUMN);
+    }
+
+    @Override
+    public void compile(ActionContext context) {
+        super.compile(context);
+    }
+    @Override
+    public void applyOnColumn(DataSetRow row, ActionContext context) {
+
+        String startValue = context.getParameters().get(START_VALUE);
+        String stepVlaue = context.getParameters().get(STEP_VALUE);
+        if (startValue.isEmpty() || stepVlaue.isEmpty()) {
+            return;
+        }
+        final String columnId = context.getColumnId();
+        Long startValueLong = Long.parseLong(startValue);
+        Long stepValueLong = Long.parseLong(stepVlaue);
+        Long currentValue = startValueLong + stepValueLong * (row.getTdpId() - 1);
+        row.set(columnId, currentValue.toString());
+    }
+}

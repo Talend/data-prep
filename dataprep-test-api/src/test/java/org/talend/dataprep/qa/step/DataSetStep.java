1f6b2a876cefebbbdd1412ee21bae4340e0f2040
yacarof <ycarof@talend.com>
2017-09-11 11:44:28 +0200

    feat(TDP-4285): First OS integ test based on api * wip.

--- dataprep-test-api/src/test/java/org/talend/dataprep/qa/step/DataSetStep.java
+++ dataprep-test-api/src/test/java/org/talend/dataprep/qa/step/DataSetStep.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.talend.dataprep.helper.DataPrepAPIHelper;
 
+import static org.junit.Assert.fail;
 
 /**
  * Step dealing with dataset
@@ -27,6 +28,12 @@ public class DataSetStep implements En {
 
         Given("^I upload the dataset \"(.*)\" with name \"(.*)\"$", (String fileName, String name) -> {
             LOG.debug("I upload the dataset {} with name {}", fileName, name);
+            try {
+                dpah.uploadDataset(fileName, name);
+            } catch (java.io.IOException ioException) {
+                LOG.error("Fail to upload file {}.", fileName, ioException);
+                fail();
+            }
         });
 
     }

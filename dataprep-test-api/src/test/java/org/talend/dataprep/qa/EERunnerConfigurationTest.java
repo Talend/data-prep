958d7c185b35a6b983579603bbfe7551c0e07843
fabballe <fabballe@talend.com>
2017-09-11 10:25:46 +0200

    Add initialization context for EE Runner

--- dataprep-test-api/src/test/java/org/talend/dataprep/qa/EERunnerConfigurationTest.java
+++ dataprep-test-api/src/test/java/org/talend/dataprep/qa/EERunnerConfigurationTest.java
@@ -2,10 +2,20 @@ package org.talend.dataprep.qa;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
+/**
+ * This runner is used to run both OS and EE specific cucumber test.
+ * Before each scenario it will log the user for EE context
+ */
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "html:target/cucumber"}, glue = "classpath:org/talend/dataprep/qa/step", features = "classpath:features/")
 public class EERunnerConfigurationTest {
+
+    @BeforeClass
+    public static void setUp(){
+        System.out.println("adding EE Context");
+    }
 
 }
--- dataprep-test-api/src/test/java/org/talend/dataprep/qa/OSRunnerConfigurationTest.java
+++ dataprep-test-api/src/test/java/org/talend/dataprep/qa/OSRunnerConfigurationTest.java
@@ -1,9 +1,13 @@
 package org.talend.dataprep.qa;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.java.Before;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
+/**
+ * This runner is used to run only OS cucumber test (do not need authentification)
+  */
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "html:target/cucumber"}, glue = "classpath:org/talend/dataprep/qa/step", features = "classpath:features/os")
 public class OSRunnerConfigurationTest {

f26128e84f09f1b946a05420be009f1021a0efc0
frassinier <frassinier@talend.com>
2017-09-22 15:20:13 +0200

    fix(TDP-4426): add others help links

--- dataprep-api/src/main/java/org/talend/dataprep/api/service/settings/actions/provider/ExternalHelpActionsProvider.java
+++ dataprep-api/src/main/java/org/talend/dataprep/api/service/settings/actions/provider/ExternalHelpActionsProvider.java
@@ -1,3 +1,16 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2017 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// https://github.com/Talend/data-prep/blob/master/LICENSE
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+
 package org.talend.dataprep.api.service.settings.actions.provider;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -5,7 +18,6 @@ import org.springframework.stereotype.Component;
 import org.talend.dataprep.api.service.settings.actions.api.ActionSettings;
 import org.talend.dataprep.help.CommunityLinksManager;
 import org.talend.dataprep.help.DocumentationLinksManager;
-import org.talend.dataprep.help.SupportLinksManager;
 
 import static org.talend.dataprep.api.service.settings.actions.api.ActionSettings.*;
 
@@ -17,9 +29,6 @@ public class ExternalHelpActionsProvider {
 
     @Autowired
     private DocumentationLinksManager documentationLinksManager;
-
-    @Autowired
-    private SupportLinksManager supportLinksManager;
 
     public ActionSettings getExternalHelpAction() {
         return builder()
@@ -39,16 +48,6 @@ public class ExternalHelpActionsProvider {
                 .type("@@external/OPEN_WINDOW")
                 .payload(PAYLOAD_METHOD_KEY, "open")
                 .payload(PAYLOAD_ARGS_KEY, new String[]{communityLinksManager.getCommunityUrl()})
-                .build();
-    }
-
-    public ActionSettings getExternalSupportAction() {
-        return builder()
-                .id("external:support")
-                .name("Support")
-                .type("@@external/OPEN_WINDOW")
-                .payload(PAYLOAD_METHOD_KEY, "open")
-                .payload(PAYLOAD_ARGS_KEY, new String[]{supportLinksManager.getSupportUrl()})
                 .build();
     }
 }

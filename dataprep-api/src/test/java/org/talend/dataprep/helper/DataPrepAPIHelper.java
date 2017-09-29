3acc65e76d49da3ce5ed9ba24babe1048321f3b8
yacarof <ycarof@talend.com>
2017-09-29 10:53:19 +0200

    feat(TDP-4014): Rollback on DataPrepAPIHelper file uploading.

--- dataprep-api/src/test/java/org/talend/dataprep/helper/DataPrepAPIHelper.java
+++ dataprep-api/src/test/java/org/talend/dataprep/helper/DataPrepAPIHelper.java
@@ -1,15 +1,11 @@
 package org.talend.dataprep.helper;
 
-import static org.talend.dataprep.helper.utils.DataPrepWebInfo.API_ACTIONS;
-import static org.talend.dataprep.helper.utils.DataPrepWebInfo.API_BACKEND_EXPORT;
-import static org.talend.dataprep.helper.utils.DataPrepWebInfo.API_DATASETS;
-import static org.talend.dataprep.helper.utils.DataPrepWebInfo.API_DETAILS;
-import static org.talend.dataprep.helper.utils.DataPrepWebInfo.API_PREPARATIONS;
-
-import java.util.Base64;
-import java.util.LinkedList;
-import java.util.List;
-
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Header;
+import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.RequestSpecification;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.talend.dataprep.helper.objects.Action;
@@ -17,11 +13,12 @@ import org.talend.dataprep.helper.objects.ActionRequest;
 import org.talend.dataprep.helper.objects.Parameters;
 import org.talend.dataprep.helper.objects.PreparationRequest;
 
-import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.http.ContentType;
-import com.jayway.restassured.response.Header;
-import com.jayway.restassured.response.Response;
-import com.jayway.restassured.specification.RequestSpecification;
+import java.nio.charset.Charset;
+import java.util.Base64;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.talend.dataprep.helper.utils.DataPrepWebInfo.*;
 
 /**
  * Utility class to allow dataprep-api integration tests.
@@ -97,7 +94,7 @@ public class DataPrepAPIHelper {
         Response response =
                 given().header(new Header("Content-Type", "text/plain"))
                         .baseUri(uploadApiBaseUrl)
-                        .body(this.getClass().getResourceAsStream(filename))
+                        .body(IOUtils.toString(DataPrepAPIHelper.class.getResourceAsStream(filename), Charset.defaultCharset()))
                         .when()
                         .queryParam("name", datasetName)
                         .post(API_DATASETS);

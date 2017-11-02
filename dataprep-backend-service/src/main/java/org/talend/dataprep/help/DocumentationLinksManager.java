ab9449c4807157c6d416ca4f7c529a95cebeada7
fabballe <fabballe@talend.com>
2017-11-02 13:19:10 +0100

    TFP-2663: update documentation i18n parameter

--- dataprep-backend-service/src/main/java/org/talend/dataprep/help/DocumentationLinksManager.java
+++ dataprep-backend-service/src/main/java/org/talend/dataprep/help/DocumentationLinksManager.java
@@ -1,11 +1,17 @@
 package org.talend.dataprep.help;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DocumentationLinksManager {
+
+    private static final String LANGUAGE_PARAMETER = "afs:lang";
+
+    @Value("${help.facets.language:}")
+    private String languageFacet;
 
     @Value("${help.facets.version:}")
     private String versionFacet;
@@ -24,6 +30,9 @@ public class DocumentationLinksManager {
     }
 
     public String getLanguageFacet() {
+        if (StringUtils.isNotEmpty(languageFacet)) {
+            return languageFacet;
+        }
         return LocaleContextHolder.getLocale().toString();
     }
 
@@ -36,6 +45,6 @@ public class DocumentationLinksManager {
     }
 
     public String getExactUrl() {
-        return exactUrl;
+        return exactUrl + "&" + LANGUAGE_PARAMETER + "=" + getLanguageFacet();
     }
 }

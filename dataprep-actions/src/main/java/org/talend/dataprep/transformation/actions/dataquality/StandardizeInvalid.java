3fc1819d2b49f69560192e7a50574380a23be5f9
qiongli <qiongli@talend.com>
2017-11-17 16:11:37 +0800

    feat(TDQ-14111 TDP-3936 ) code improvement after review

--- dataprep-actions/src/main/java/org/talend/dataprep/transformation/actions/dataquality/StandardizeInvalid.java
+++ dataprep-actions/src/main/java/org/talend/dataprep/transformation/actions/dataquality/StandardizeInvalid.java
@@ -14,13 +14,10 @@
 package org.talend.dataprep.transformation.actions.dataquality;
 
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.talend.dataprep.api.action.Action;
 import org.talend.dataprep.api.dataset.ColumnMetadata;
 import org.talend.dataprep.api.dataset.RowMetadata;
 import org.talend.dataprep.api.dataset.row.DataSetRow;
-import org.talend.dataprep.api.dataset.statistics.SemanticDomain;
 import org.talend.dataprep.i18n.ActionsBundle;
 import org.talend.dataprep.parameters.Parameter;
 import org.talend.dataprep.parameters.SelectParameter;
@@ -29,11 +26,11 @@ import org.talend.dataprep.transformation.actions.common.AbstractActionMetadata;
 import org.talend.dataprep.transformation.actions.common.ColumnAction;
 import org.talend.dataprep.transformation.api.action.context.ActionContext;
 import org.talend.dataquality.semantic.api.CategoryRegistryManager;
-import org.talend.dataquality.semantic.index.DictionarySearchMode;
 import org.talend.dataquality.semantic.index.LuceneIndex;
+import org.talend.dataquality.semantic.model.CategoryType;
+import org.talend.dataquality.semantic.model.DQCategory;
+import org.talend.dataquality.semantic.recognizer.CategoryRecognizerBuilder;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.*;
 
 import static org.talend.dataprep.transformation.actions.category.ActionScope.INVALID;
@@ -44,7 +41,6 @@ import static org.talend.dataprep.transformation.actions.category.ActionScope.IN
 @Action(AbstractActionMetadata.ACTION_BEAN_PREFIX + StandardizeInvalid.ACTION_NAME)
 public class StandardizeInvalid extends AbstractActionMetadata implements ColumnAction {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(StandardizeInvalid.class);
     /**
      * Action name.
      */
@@ -53,33 +49,32 @@ public class StandardizeInvalid extends AbstractActionMetadata implements Column
     /**
      * parameter of matching threshold
      */
-    protected static final String MATCH_THRESHOLD_PARAMETER = "match_threshold";
+    private static final String MATCH_THRESHOLD_PARAMETER = "match_threshold";
 
     /**
      * User selected match threshold
      */
-    protected static final String MATCH_THRESHOLD_KEY = "match_threshold_key";
+    private static final String MATCH_THRESHOLD_KEY = "match_threshold_key";
 
     /**
      * The selected column if it uses semantic category
      */
-    protected static final String COLUMN_USE_SEMANTIC_KEY = "use_semantic_category";
+    private static final String COLUMN_IS_SEMANTIC_KEY = "is_semantic_category";
 
-    protected static final String LUCENE_INDEX_KEY = "lucene_index";
+    private static final String LUCENE_INDEX_KEY = "lucene_index";
 
-    protected static final List<String> ACTION_SCOPE = Collections.singletonList(INVALID.getDisplayName());
-
+    private static final List<String> ACTION_SCOPE = Collections.singletonList(INVALID.getDisplayName());
 
     @Override
     public String getName() {
         return ACTION_NAME;
     }
 
-
     @Override
     public List<Parameter> getParameters() {
         final List<Parameter> parameters = super.getParameters();
-        Parameter startParameter = SelectParameter.Builder.builder()
+        Parameter startParameter = SelectParameter.Builder
+                .builder()
                 .name(MATCH_THRESHOLD_PARAMETER)
                 .item(MatchThresholdEnum.HIGH.name(), MatchThresholdEnum.HIGH.getLabel())
                 .item(MatchThresholdEnum.DEFAULT.name(), MatchThresholdEnum.DEFAULT.getLabel())
@@ -97,18 +92,12 @@ public class StandardizeInvalid extends AbstractActionMetadata implements Column
             String matchThresholdPara = actionContext.getParameters().get(MATCH_THRESHOLD_PARAMETER);
             Double thresholdValue = MatchThresholdEnum.valueOf(matchThresholdPara).getThreshold();
             actionContext.get(MATCH_THRESHOLD_KEY, p -> thresholdValue);
-            try {
-                final URI ddPath = CategoryRegistryManager.getInstance().getDictionaryURI();
-                actionContext.get(LUCENE_INDEX_KEY, p -> new LuceneIndex(ddPath, DictionarySearchMode.MATCH_SEMANTIC_DICTIONARY));
-            } catch (URISyntaxException exc) {
-                LOGGER.error(exc.getMessage(), exc);
-                actionContext.setActionStatus(ActionContext.ActionStatus.CANCELED);
-            }
-            //this action only apply for column uses Semantic category.
+            actionContext.get(LUCENE_INDEX_KEY, p -> CategoryRecognizerBuilder.newBuilder().getSharedDataDictIndex());
+            // this action only apply for column uses Semantic category.
             final RowMetadata rowMetadata = actionContext.getRowMetadata();
             final String columnId = actionContext.getColumnId();
             final ColumnMetadata column = rowMetadata.getById(columnId);
-            actionContext.get(COLUMN_USE_SEMANTIC_KEY, p -> isUseSemanticCategory(column));
+            actionContext.get(COLUMN_IS_SEMANTIC_KEY, p -> isDictionaryType(column));
         }
     }
 
@@ -124,91 +113,65 @@ public class StandardizeInvalid extends AbstractActionMetadata implements Column
 
     @Override
     public boolean acceptField(ColumnMetadata column) {
-        boolean useSemanticCategory = isUseSemanticCategory(column);
-        return useSemanticCategory;
+        return isDictionaryType(column);
     }
-
 
     @Override
     public void applyOnColumn(DataSetRow row, ActionContext context) {
-        //Return original value when the semantic domain is empty or the column not use semantic
-        boolean isColumnUseSemantic = context.get(COLUMN_USE_SEMANTIC_KEY);
+        // Return original value when the column not use semantic
+        boolean isColumnUseSemantic = context.get(COLUMN_IS_SEMANTIC_KEY);
         if (!isColumnUseSemantic) {
             return;
         }
         final String columnId = context.getColumnId();
         final String value = row.get(columnId);
-        //Apply on none-empty and invalid value.
+        // Apply on none-empty and invalid value.
         if (StringUtils.isEmpty(value) || !row.isInvalid(columnId)) {
             return;
         }
         final RowMetadata rowMetadata = context.getRowMetadata();
         final ColumnMetadata column = rowMetadata.getById(columnId);
 
-
         final LuceneIndex luceneIndex = context.get(LUCENE_INDEX_KEY);
         final Double threshold = context.get(MATCH_THRESHOLD_KEY);
         Map<String, Double> similarMap = luceneIndex.findSimilarFieldsInCategory(value, column.getDomain(), threshold);
-        //If not found the similar value, display original value.
-        if (similarMap.isEmpty()) {
-            return;
-        }
-        //The similarMap is sort by value(score) on DQ lib, so the first element is the highest score.
-        String closestValue = getFirstKey(similarMap);
+        String closestValue = luceneIndex.findMostSimilarFieldInCategory(value, column.getDomain(), threshold);
+        // If not found the similar value, display original value.
         if (!StringUtils.isEmpty(closestValue)) {
             row.set(columnId, closestValue);
         }
-
     }
 
-    private boolean isUseSemanticCategory(ColumnMetadata column) {
+    private boolean isDictionaryType(ColumnMetadata column) {
         String domain = column.getDomain();
         if (StringUtils.isEmpty(domain)) {
             return false;
         }
-        List<SemanticDomain> semanticDomains = column.getSemanticDomains();
-        for (SemanticDomain semDomain : semanticDomains) {
-            if (domain.equals(semDomain.getId())) {
-                return true;
-            }
+        DQCategory category = CategoryRegistryManager.getInstance().getCategoryMetadataByName(domain);
+        if (category != null && CategoryType.DICT.equals(category.getType())) {
+            return true;
         }
         return false;
     }
-
-    private String getFirstKey(Map<String, Double> similarMap) {
-        String closestValue = null;
-        Set<String> keySet = similarMap.keySet();
-        Iterator<String> iterator = keySet.iterator();
-        while (iterator.hasNext()) {
-            String next = iterator.next();
-            if (next != null) {
-                closestValue = next;
-                break;
-            }
-        }
-        return closestValue;
-    }
-
 
     @Override
     public Set<Behavior> getBehavior() {
         return EnumSet.of(Behavior.VALUES_COLUMN, Behavior.NEED_STATISTICS_INVALID);
     }
 
-    public enum MatchThresholdEnum {
+    private enum MatchThresholdEnum {
         HIGH("high_match", 0.9),
         DEFAULT("default_match", 0.8),
         NONE("none_match", 0.0);
+
         private String label;
 
         private Double threshold;
-
 
         MatchThresholdEnum(String label, Double threshold) {
             this.label = label;
             this.threshold = threshold;
         }
-
 
         public Double getThreshold() {
             return threshold;
--- dataprep-actions/src/test/java/org/talend/dataprep/transformation/actions/dataquality/StandardizeInvalidTest.java
+++ dataprep-actions/src/test/java/org/talend/dataprep/transformation/actions/dataquality/StandardizeInvalidTest.java
@@ -1,15 +1,15 @@
-//  ============================================================================
+// ============================================================================
 //
-//  Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
 //
-//  This source code is available under agreement available at
-//  https://github.com/Talend/data-prep/blob/master/LICENSE
+// This source code is available under agreement available at
+// https://github.com/Talend/data-prep/blob/master/LICENSE
 //
-//  You should have received a copy of the agreement
-//  along with this program; if not, write to Talend SA
-//  9 rue Pages 92150 Suresnes, France
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
 //
-//  ============================================================================
+// ============================================================================
 package org.talend.dataprep.transformation.actions.dataquality;
 
 import org.junit.Before;
@@ -22,26 +22,17 @@ import org.talend.dataprep.api.dataset.statistics.SemanticDomain;
 import org.talend.dataprep.api.type.Type;
 import org.talend.dataprep.parameters.Parameter;
 import org.talend.dataprep.transformation.actions.AbstractMetadataBaseTest;
-import org.talend.dataprep.transformation.actions.ActionMetadataTestUtils;
 import org.talend.dataprep.transformation.actions.category.ActionCategory;
 import org.talend.dataprep.transformation.actions.common.ImplicitParameters;
-import org.talend.dataprep.transformation.actions.common.RunnableAction;
-import org.talend.dataprep.transformation.actions.fill.GenerateSequence;
-import org.talend.dataprep.transformation.actions.text.LowerCase;
 import org.talend.dataprep.transformation.api.action.ActionTestWorkbench;
-import org.talend.dataprep.transformation.api.action.context.ActionContext;
-import org.talend.dataprep.transformation.api.action.context.TransformationContext;
 import org.talend.dataquality.semantic.classifier.SemanticCategoryEnum;
 
-import java.io.IOException;
 import java.util.*;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import static org.talend.dataprep.api.dataset.ColumnMetadata.Builder.column;
-import static org.talend.dataprep.transformation.actions.AbstractMetadataBaseTest.ValueBuilder.value;
-import static org.talend.dataprep.transformation.actions.AbstractMetadataBaseTest.ValuesBuilder.builder;
-import static org.talend.dataprep.transformation.actions.ActionMetadataTestUtils.getColumn;
+import static org.talend.dataprep.transformation.actions.category.ActionScope.INVALID;
 
 /**
  * Test class for StandardizeInvalid action
@@ -50,11 +41,14 @@ import static org.talend.dataprep.transformation.actions.ActionMetadataTestUtils
  */
 public class StandardizeInvalidTest extends AbstractMetadataBaseTest {
 
+    private final String MATCH_THRESHOLD_PARAMETER = "match_threshold";
+
+    private final List<String> ACTION_SCOPE = Collections.singletonList(INVALID.getDisplayName());
+
     /**
      * The action to test.
      */
     private StandardizeInvalid standardizeInvalid;
-
 
     private Map<String, String> parameters;
 
@@ -64,7 +58,7 @@ public class StandardizeInvalidTest extends AbstractMetadataBaseTest {
         parameters = new HashMap<>();
         parameters.put(ImplicitParameters.SCOPE.getKey().toLowerCase(), "column");
         parameters.put(ImplicitParameters.COLUMN_ID.getKey().toLowerCase(), "0001");
-        parameters.put(StandardizeInvalid.MATCH_THRESHOLD_PARAMETER, StandardizeInvalid.MatchThresholdEnum.DEFAULT.name());
+        parameters.put(MATCH_THRESHOLD_PARAMETER, "DEFAULT");
 
     }
 
@@ -98,34 +92,13 @@ public class StandardizeInvalidTest extends AbstractMetadataBaseTest {
         final RowMetadata rowMetadata = row.getRowMetadata();
         rowMetadata.getById("0001").setDomain("COUNTRY");
         rowMetadata.getById("0001").setType(Type.STRING.getName());
-        List<SemanticDomain> semanticDomains = new ArrayList<SemanticDomain>();
+        List<SemanticDomain> semanticDomains = new ArrayList<>();
         semanticDomains.add(new SemanticDomain("COUNTRY", "Country", 0.85f));
         rowMetadata.getById("0001").setSemanticDomains(semanticDomains);
 
         final Map<String, Object> expectedValues = new LinkedHashMap<>();
         expectedValues.put("0000", "David Bowie");
         expectedValues.put("0001", "Russian Federation");
-        expectedValues.put("__tdpInvalid", "0001");
-
-        // when
-        ActionTestWorkbench.test(row, actionRegistry, factory.create(standardizeInvalid, parameters));
-
-        // then
-        assertEquals(expectedValues, row.values());
-    }
-
-    @Test
-    public void should_not_standardize_no_semantic() {
-        // given
-        final Map<String, String> values = new HashMap<>();
-        values.put("0000", "David Bowie");
-        values.put("0001", "Russian Federatio");
-
-        final DataSetRow row = new DataSetRow(values);
-        row.setInvalid("0001");
-        final Map<String, Object> expectedValues = new LinkedHashMap<>();
-        expectedValues.put("0000", "David Bowie");
-        expectedValues.put("0001", "Russian Federatio");
         expectedValues.put("__tdpInvalid", "0001");
 
         // when
@@ -166,10 +139,10 @@ public class StandardizeInvalidTest extends AbstractMetadataBaseTest {
         values.put("0000", "David Bowie");
         values.put("0001", "Russie");
 
-        //set semantic domain
+        // set semantic domain
         final DataSetRow row = new DataSetRow(values);
         final RowMetadata rowMetadata = row.getRowMetadata();
-        List<SemanticDomain> semanticDomains = new ArrayList<SemanticDomain>();
+        List<SemanticDomain> semanticDomains = new ArrayList<>();
         semanticDomains.add(new SemanticDomain("COUNTRY", "Country", 0.85f));
         rowMetadata.getById("0001").setSemanticDomains(semanticDomains);
         rowMetadata.getById("0001").setType(Type.STRING.getName());
@@ -189,17 +162,17 @@ public class StandardizeInvalidTest extends AbstractMetadataBaseTest {
     @Test
     public void should_not_standardize_out_of_threshold() {
         // given
-        parameters.put(StandardizeInvalid.MATCH_THRESHOLD_PARAMETER, StandardizeInvalid.MatchThresholdEnum.HIGH.name());
+        parameters.put(MATCH_THRESHOLD_PARAMETER, "HIGH");
         final Map<String, String> values = new HashMap<>();
         values.put("0000", "David Bowie");
         values.put("0001", "Ferrand");
 
         final DataSetRow row = new DataSetRow(values);
         row.setInvalid("0001");
-        //set semantic domain
+        // set semantic domain
         final RowMetadata rowMetadata = row.getRowMetadata();
         rowMetadata.getById("0001").setDomain("FR_COMMUNE");
-        List<SemanticDomain> semanticDomains = new ArrayList<SemanticDomain>();
+        List<SemanticDomain> semanticDomains = new ArrayList<>();
         semanticDomains.add(new SemanticDomain("FR_COMMUNE", "Fr_Commune", 0.85f));
         rowMetadata.getById("0001").setSemanticDomains(semanticDomains);
         rowMetadata.getById("0001").setType(Type.STRING.getName());
@@ -223,13 +196,13 @@ public class StandardizeInvalidTest extends AbstractMetadataBaseTest {
         values.put("0000", "David Bowie");
         values.put("0001", "");
 
-        //set semantic domain
+        // set semantic domain
         final DataSetRow row = new DataSetRow(values);
         row.setInvalid("0001");
         final RowMetadata rowMetadata = row.getRowMetadata();
         rowMetadata.getById("0001").setDomain("COUNTRY");
         rowMetadata.getById("0001").setType(Type.STRING.getName());
-        List<SemanticDomain> semanticDomains = new ArrayList<SemanticDomain>();
+        List<SemanticDomain> semanticDomains = new ArrayList<>();
         semanticDomains.add(new SemanticDomain("COUNTRY", "Country", 0.85f));
         rowMetadata.getById("0001").setSemanticDomains(semanticDomains);
 
@@ -248,25 +221,30 @@ public class StandardizeInvalidTest extends AbstractMetadataBaseTest {
     @Test
     public void should_action_Scope() {
         assertTrue(standardizeInvalid.getActionScope().size() == 1);
-        assertTrue(standardizeInvalid.getActionScope().equals(standardizeInvalid.ACTION_SCOPE));
+        assertTrue(standardizeInvalid.getActionScope().equals(ACTION_SCOPE));
     }
 
     @Test
     public void should_accept_column() {
-        //a column with semantic
+        // a column with semantic
         SemanticCategoryEnum semantic = SemanticCategoryEnum.COUNTRY;
-        List semanticDomainLs = new ArrayList();
+        List<SemanticDomain> semanticDomainLs = new ArrayList<>();
         semanticDomainLs.add(new SemanticDomain("COUNTRY", "Country", 0.85f));
-        ColumnMetadata column = ColumnMetadata.Builder.column().id(0).name("name").type(Type.STRING).semanticDomains(semanticDomainLs).domain(semantic.name()).build();
+        ColumnMetadata column = ColumnMetadata.Builder
+                .column()
+                .id(0)
+                .name("name")
+                .type(Type.STRING)
+                .semanticDomains(semanticDomainLs)
+                .domain(semantic.name())
+                .build();
         assertTrue(standardizeInvalid.acceptField(column));
     }
 
     @Test
     public void should_not_accept_column() {
-        //no semantic
+        // no semantic
         ColumnMetadata column = ColumnMetadata.Builder.column().id(0).name("name").type(Type.STRING).build();
-        assertFalse(standardizeInvalid.acceptField(column));
-        column = ColumnMetadata.Builder.column().id(0).name("name").type(Type.STRING).domain("COUNTRY").build();
         assertFalse(standardizeInvalid.acceptField(column));
     }
 

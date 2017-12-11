9170f8f05fec843a48fc7800243d1ca5ad21ea2f
Henri-Mayeul de Benque <hmdebenque@talend.com>
2017-12-11 17:27:29 +0100

    feat(TDP-3798): format

--- dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/pipeline/builder/ActionsStaticProfiler.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/transformation/pipeline/builder/ActionsStaticProfiler.java
@@ -119,7 +119,11 @@ class ActionsStaticProfiler {
     }
 
     private static boolean isCreateColumnParameterOn(Action action) {
-        return action.getParameters().entrySet().stream().anyMatch(e -> Objects.equals(e.getKey(), CREATE_NEW_COLUMN) && Boolean.parseBoolean(e.getValue()));
+        return action //
+                .getParameters() //
+                .entrySet() //
+                .stream() //
+                .anyMatch(e -> Objects.equals(e.getKey(), CREATE_NEW_COLUMN) && Boolean.parseBoolean(e.getValue()));
     }
 
     private static class FilterForFullAnalysis implements SerializablePredicate<ColumnMetadata> {

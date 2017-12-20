34414ddf496d6588eb8a52daed416450305572c9
Nicolas Comont <ncomont@talend.com>
2017-12-20 15:12:38 +0100

    feat(TDP-4884): use ui notifications instead of ours

--- dataprep-webapp/src/app/services/message/message-service.spec.js
+++ dataprep-webapp/src/app/services/message/message-service.spec.js
@@ -14,7 +14,7 @@
 describe('Message service', () => {
     'use strict';
 
-    beforeEach(angular.mock.module('data-prep.services.utils'));
+    beforeEach(angular.mock.module('data-prep.services.message'));
 
     beforeEach(angular.mock.module('pascalprecht.translate', ($translateProvider) => {
         $translateProvider.translations('en_US', {
@@ -25,111 +25,105 @@ describe('Message service', () => {
         $translateProvider.preferredLanguage('en_US');
     }));
 
-    beforeEach(inject((toaster) => {
-        spyOn(toaster, 'pop').and.returnValue();
+    beforeEach(inject((StateService) => {
+        spyOn(StateService, 'pushMessage').and.returnValue();
     }));
 
     describe('error', () => {
-        it('should show toast on error without translate arg', inject(($timeout, MessageService, toaster) => {
+        it('should show toast on error without translate arg', inject((MessageService, StateService) => {
             //given
             var titleId = 'TITLE';
             var contentId = 'CONTENT_WITHOUT_ARG';
 
-            expect(toaster.pop).not.toHaveBeenCalled();
+            expect(StateService.pushMessage).not.toHaveBeenCalled();
 
             //when
             MessageService.error(titleId, contentId);
-            $timeout.flush(300);
 
             //then
-            expect(toaster.pop).toHaveBeenCalledWith('error', 'TITLE_VALUE', 'CONTENT_WITHOUT_ARG_VALUE', 0);
+            expect(StateService.pushMessage).toHaveBeenCalledWith('error', 'TITLE_VALUE', 'CONTENT_WITHOUT_ARG_VALUE');
         }));
 
-        it('should show toast on error with translate arg', inject(($timeout, MessageService, toaster) => {
+        it('should show toast on error with translate arg', inject((MessageService, StateService) => {
             //given
             var titleId = 'TITLE';
             var contentId = 'CONTENT_WITH_ARG';
             var args = { argValue: 'my value' };
 
-            expect(toaster.pop).not.toHaveBeenCalled();
+            expect(StateService.pushMessage).not.toHaveBeenCalled();
 
             //when
             MessageService.error(titleId, contentId, args);
-            $timeout.flush(300);
 
             //then
-            expect(toaster.pop).toHaveBeenCalledWith('error', 'TITLE_VALUE', 'CONTENT_WITH_ARG_VALUE : my value', 0);
+            expect(StateService.pushMessage).toHaveBeenCalledWith('error', 'TITLE_VALUE', 'CONTENT_WITH_ARG_VALUE : my value');
         }));
     });
 
     describe('warning', () => {
-        it('should show toast on warning without translate arg', inject(($timeout, MessageService, toaster) => {
+        it('should show toast on warning without translate arg', inject((MessageService, StateService) => {
             //given
             var titleId = 'TITLE';
             var contentId = 'CONTENT_WITHOUT_ARG';
 
-            expect(toaster.pop).not.toHaveBeenCalled();
+            expect(StateService.pushMessage).not.toHaveBeenCalled();
 
             //when
             MessageService.warning(titleId, contentId);
-            $timeout.flush(300);
 
             //then
-            expect(toaster.pop).toHaveBeenCalledWith('warning', 'TITLE_VALUE', 'CONTENT_WITHOUT_ARG_VALUE', 0);
+            expect(StateService.pushMessage).toHaveBeenCalledWith('warning', 'TITLE_VALUE', 'CONTENT_WITHOUT_ARG_VALUE');
         }));
 
-        it('should show toast on warning with translate arg', inject(($timeout, MessageService, toaster) => {
+        it('should show toast on warning with translate arg', inject((MessageService, StateService) => {
             //given
             var titleId = 'TITLE';
             var contentId = 'CONTENT_WITH_ARG';
             var args = { argValue: 'my value' };
 
-            expect(toaster.pop).not.toHaveBeenCalled();
+            expect(StateService.pushMessage).not.toHaveBeenCalled();
 
             //when
             MessageService.warning(titleId, contentId, args);
-            $timeout.flush(300);
 
             //then
-            expect(toaster.pop).toHaveBeenCalledWith('warning', 'TITLE_VALUE', 'CONTENT_WITH_ARG_VALUE : my value', 0);
+            expect(StateService.pushMessage).toHaveBeenCalledWith('warning', 'TITLE_VALUE', 'CONTENT_WITH_ARG_VALUE : my value');
         }));
     });
 
     describe('success', () => {
-        it('should show toast on success without translate arg', inject(($timeout, MessageService, toaster) => {
+        it('should show toast on success without translate arg', inject((MessageService, StateService) => {
             //given
             var titleId = 'TITLE';
             var contentId = 'CONTENT_WITHOUT_ARG';
 
-            expect(toaster.pop).not.toHaveBeenCalled();
+            expect(StateService.pushMessage).not.toHaveBeenCalled();
 
             //when
             MessageService.success(titleId, contentId);
-            $timeout.flush(300);
 
             //then
-            expect(toaster.pop).toHaveBeenCalledWith('success', 'TITLE_VALUE', 'CONTENT_WITHOUT_ARG_VALUE', 5000);
+            expect(StateService.pushMessage).toHaveBeenCalledWith('success', 'TITLE_VALUE', 'CONTENT_WITHOUT_ARG_VALUE');
         }));
 
-        it('should show toast on success with translate arg', inject(($timeout, MessageService, toaster) => {
+        it('should show toast on success with translate arg', inject((MessageService, StateService) => {
             //given
             var titleId = 'TITLE';
             var contentId = 'CONTENT_WITH_ARG';
             var args = { argValue: 'my value' };
 
-            expect(toaster.pop).not.toHaveBeenCalled();
+            expect(StateService.pushMessage).not.toHaveBeenCalled();
 
             //when
             MessageService.success(titleId, contentId, args);
-            $timeout.flush(300);
 
             //then
-            expect(toaster.pop).toHaveBeenCalledWith('success', 'TITLE_VALUE', 'CONTENT_WITH_ARG_VALUE : my value', 5000);
+            expect(StateService.pushMessage).toHaveBeenCalledWith('success', 'TITLE_VALUE', 'CONTENT_WITH_ARG_VALUE : my value');
         }));
     });
 
     describe('buffer', () => {
-        it('should buffer different messages and display them after 300ms delay', inject(($timeout, MessageService, toaster) => {
+        it('should buffer different messages and display them after 300ms delay', inject((MessageService, StateService) => {
             //given
             var firstMessageTitleId = 'TITLE';
             var firstMessageContentId = 'CONTENT_WITH_ARG';
@@ -138,20 +132,19 @@ describe('Message service', () => {
             var secondMessageTitleId = 'TITLE';
             var secondMessageContentId = 'CONTENT_WITHOUT_ARG';
 
-            expect(toaster.pop).not.toHaveBeenCalled();
+            expect(StateService.pushMessage).not.toHaveBeenCalled();
 
             //when
             MessageService.success(firstMessageTitleId, firstMessageContentId, firstMessageArgs);
             MessageService.error(secondMessageTitleId, secondMessageContentId);
-            expect(toaster.pop).not.toHaveBeenCalled();
-            $timeout.flush(300);
+            expect(StateService.pushMessage).not.toHaveBeenCalled();
 
             //then
-            expect(toaster.pop).toHaveBeenCalledWith('success', 'TITLE_VALUE', 'CONTENT_WITH_ARG_VALUE : my value', 5000);
-            expect(toaster.pop).toHaveBeenCalledWith('error', 'TITLE_VALUE', 'CONTENT_WITHOUT_ARG_VALUE', 0);
+            expect(StateService.pushMessage).toHaveBeenCalledWith('success', 'TITLE_VALUE', 'CONTENT_WITH_ARG_VALUE : my value');
+            expect(StateService.pushMessage).toHaveBeenCalledWith('error', 'TITLE_VALUE', 'CONTENT_WITHOUT_ARG_VALUE');
         }));
 
-        it('should only display distinct message once', inject(($timeout, MessageService, toaster) => {
+        it('should only display distinct message once', inject((MessageService, StateService) => {
             //given
             var firstMessageTitleId = 'TITLE';
             var firstMessageContentId = 'CONTENT_WITH_ARG';
@@ -160,19 +153,18 @@ describe('Message service', () => {
             var secondMessageTitleId = 'TITLE';
             var secondMessageContentId = 'CONTENT_WITHOUT_ARG';
 
-            expect(toaster.pop).not.toHaveBeenCalled();
+            expect(StateService.pushMessage).not.toHaveBeenCalled();
 
             //when
             MessageService.success(firstMessageTitleId, firstMessageContentId, firstMessageArgs);
             MessageService.success(firstMessageTitleId, firstMessageContentId, firstMessageArgs);
             MessageService.success(firstMessageTitleId, firstMessageContentId, firstMessageArgs);
             MessageService.error(secondMessageTitleId, secondMessageContentId);
-            $timeout.flush(300);
 
             //then
-            expect(toaster.pop.calls.count()).toBe(2);
-            expect(toaster.pop).toHaveBeenCalledWith('success', 'TITLE_VALUE', 'CONTENT_WITH_ARG_VALUE : my value', 5000);
-            expect(toaster.pop).toHaveBeenCalledWith('error', 'TITLE_VALUE', 'CONTENT_WITHOUT_ARG_VALUE', 0);
+            expect(StateService.pushMessage.calls.count()).toBe(2);
+            expect(StateService.pushMessage).toHaveBeenCalledWith('success', 'TITLE_VALUE', 'CONTENT_WITH_ARG_VALUE : my value');
+            expect(StateService.pushMessage).toHaveBeenCalledWith('error', 'TITLE_VALUE', 'CONTENT_WITHOUT_ARG_VALUE');
         }));
     });
 });

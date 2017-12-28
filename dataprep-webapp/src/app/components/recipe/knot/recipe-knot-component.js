bc1eaec2da16031233d5e07a54e7a99c424cc36a
charlesnguyen <cnguyen@talend.com>
2017-12-28 16:08:28 +0100

    fix(TDP-4620): Add tooltip when hovering recipe bullet

--- dataprep-webapp/src/app/components/recipe/knot/recipe-knot-component.js
+++ dataprep-webapp/src/app/components/recipe/knot/recipe-knot-component.js
@@ -54,8 +54,16 @@ const RecipeKnot = {
         ng-mouseover="$ctrl.onHoverStart()"
         ng-mouseleave="$ctrl.onHoverEnd()">
             <div class="line" ng-class="{'no-line': !$ctrl.showTopLine}"></div>
-            <div ng-if="$ctrl.inactive" class="circle inactive-knot" ng-class="{'to-be-activated': $ctrl.toBeSwitched}"></div>
-            <div ng-if="!$ctrl.inactive" class="circle" ng-class="{'to-be-deactivated': $ctrl.toBeSwitched}"></div>
+            <div 
+            	ng-if="$ctrl.inactive" 
+            	class="circle inactive-knot" 
+            	ng-class="{'to-be-activated': $ctrl.toBeSwitched}"
+            	title="{{::'ENABLE_STEP' | translate }}"></div>
+            <div 
+            	ng-if="!$ctrl.inactive" 
+            	class="circle" 
+            	ng-class="{'to-be-deactivated': $ctrl.toBeSwitched}"
+            	title="{{::'DISABLE_STEP' | translate }}"></div>
             <div class="line bottom-line" ng-class="{'no-line': !$ctrl.showBottomLine}"></div>
     <div>`,
 };

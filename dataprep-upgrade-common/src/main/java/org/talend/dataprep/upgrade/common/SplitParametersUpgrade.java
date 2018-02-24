ead7048b2a856dc6b91dc4a5351a0579b04a02b7
fhuaulme <fhuaulme@talend.com>
2018-02-24 19:43:40 +0100

    fix: Add Split parameter migration
    * Initial commit for migration.
    * Put common code to same place.

--- dataprep-upgrade-common/src/main/java/org/talend/dataprep/upgrade/common/SplitParametersUpgrade.java
+++ dataprep-upgrade-common/src/main/java/org/talend/dataprep/upgrade/common/SplitParametersUpgrade.java
@@ -15,24 +15,30 @@ package org.talend.dataprep.upgrade.common;
 
 import static org.talend.tql.api.TqlBuilder.eq;
 
-import java.util.Locale;
+import java.util.Map;
 
 import org.talend.dataprep.api.preparation.Action;
 import org.talend.dataprep.preparation.store.PreparationRepository;
-import org.talend.dataprep.transformation.actions.date.ExtractDateTokens;
+import org.talend.dataprep.transformation.actions.text.Split;
 
-public class AddLanguageToActionExtractDatePartUpgrade {
+public class SplitParametersUpgrade {
 
-    private AddLanguageToActionExtractDatePartUpgrade() {
+    private SplitParametersUpgrade() {
     }
 
     public static void upgradeActions(PreparationRepository preparationRepository) {
         ParameterMigration.upgradeParameters(preparationRepository, eq("actions.action", //
-                ExtractDateTokens.ACTION_NAME), //
-                AddLanguageToActionExtractDatePartUpgrade::updateAction);
+                Split.SPLIT_ACTION_NAME), //
+                SplitParametersUpgrade::updateAction);
     }
 
     private static void updateAction(Action action) {
-        action.getParameters().put(ExtractDateTokens.LANGUAGE, Locale.getDefault().getLanguage());
+        final Map<String, String> parameters = action.getParameters();
+        final String separator = parameters.get(Split.SEPARATOR_PARAMETER);
+        if (Split.OLD_OTHER_REGEX.equals(separator)) {
+            parameters.put(Split.SEPARATOR_PARAMETER, Split.OTHER_REGEX);
+        } else if (Split.OLD_OTHER_STRING.equals(separator)) {
+            parameters.put(Split.SEPARATOR_PARAMETER, Split.OTHER_STRING);
+        }
     }
 }

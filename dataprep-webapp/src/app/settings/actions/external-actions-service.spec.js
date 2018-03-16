4c8b51a35bd5773ffaec229420fd383f237a4ba9
Nicolas Comont <ncomont@talend.com>
2018-03-16 15:42:18 +0100

    fix: tests

--- dataprep-webapp/src/app/settings/actions/external-actions-service.spec.js
+++ dataprep-webapp/src/app/settings/actions/external-actions-service.spec.js
@@ -32,7 +32,7 @@ describe('External actions service', () => {
 
 			ExternalActionsService.dispatch(action);
 
-			expect($window.location.href).toEqual('https://1.2/3/4?redirect=https://a.b/c/d');
+			expect($window.location.href).toEqual('https://1.2/3/4?redirect=https%3A%2F%2Fa.b%2Fc%2Fd%3Fe%3Df%26g%3Dh');
 		}));
 
 		it('should open window', inject(($window, ExternalActionsService) => {

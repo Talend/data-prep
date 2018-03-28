062b50a17e7ec39af437d6183a0fb8e6d8ba1a63
cgall <cgall@talend.com>
2018-03-28 17:01:01 +0200

    TDP-1461: Add delete all empty columns action * edit label and desc of delete all empty action

---
+++ dataprep-actions/src/main/java/org/talend/dataprep/transformation/actions/delete/DeleteAllEmptyColumns.java
@@ -0,0 +1,92 @@
+package org.talend.dataprep.transformation.actions.delete;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.talend.dataprep.api.dataset.ColumnMetadata;
+import org.talend.dataprep.api.dataset.Quality;
+import org.talend.dataprep.api.dataset.row.DataSetRow;
+import org.talend.dataprep.parameters.Parameter;
+import org.talend.dataprep.parameters.SelectParameter;
+import org.talend.dataprep.transformation.actions.column.DeleteColumn;
+import org.talend.dataprep.transformation.actions.common.AbstractActionMetadata;
+import org.talend.dataprep.transformation.actions.common.DataSetAction;
+import org.talend.dataprep.transformation.api.action.context.ActionContext;
+
+import java.util.*;
+
+import static org.talend.dataprep.transformation.actions.category.ActionCategory.DATA_CLEANSING;
+
+public class DeleteAllEmptyColumns extends AbstractActionMetadata implements DataSetAction {
+
+    /**
+     * The action name.
+     */
+    public static final String DELETE_ALL_EMPTY_COLUMNS_ACTION_NAME = "delete_all_empty_columns";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DeleteColumn.class);
+
+    protected static final String ACTION_PARAMETER = "action_on_columns_with_blank";
+
+    protected static final String DELETE = "delete";
+
+    protected static final String KEEP = "keep";
+
+    @Override
+    public String getName() {
+        return DELETE_ALL_EMPTY_COLUMNS_ACTION_NAME;
+    }
+
+    @Override
+    public List<Parameter> getParameters(Locale locale) {
+        List<Parameter> parameters = super.getParameters(locale);
+
+        parameters.add(SelectParameter.selectParameter(locale)
+                .name(ACTION_PARAMETER)
+                .item(DELETE, DELETE)
+                .item(KEEP, KEEP)
+                .defaultValue(DELETE)
+                .build(this));
+
+        return parameters;
+    }
+
+    @Override
+    public String getCategory(Locale locale) {
+        return DATA_CLEANSING.getDisplayName(locale);
+    }
+
+    @Override
+    public boolean acceptField(ColumnMetadata column) {
+        return true;
+    }
+
+    @Override
+    public void applyOnDataSet(DataSetRow row, ActionContext context) {
+        String columnId;
+        ColumnMetadata column;
+        Quality quality;
+        List<ColumnMetadata> columns = row.getRowMetadata().getColumns();
+
+        for (int i = columns.size() - 1; i >= 0; i--) {
+            column = columns.get(i);
+            columnId = column.getId();
+            if (context.getParameters().get(ACTION_PARAMETER).equals(KEEP)) {
+                if (context.getRowMetadata().getById(columnId).getStatistics().getDataFrequencies().size() > 1) {
+                    continue; //parameter to keep blanks is at true and the size of the datafrequencies is higher than 1 so we don't delete this column
+                }
+            }
+            quality = column.getQuality();
+            if (quality.getValid() + quality.getInvalid() == 0) {
+                LOGGER.debug("DeleteColumn for columnId {}", columnId);
+                context.getRowMetadata().deleteColumnById(columnId);
+                row.deleteColumnById(columnId);
+            }
+        }
+    }
+
+    @Override
+    public Set<Behavior> getBehavior() {
+        return EnumSet.of(Behavior.METADATA_DELETE_COLUMNS);
+    }
+
+}
---
+++ dataprep-actions/src/test/java/org/talend/dataprep/transformation/actions/delete/DeleteAllEmptyColumnsTest.java
@@ -0,0 +1,157 @@
+package org.talend.dataprep.transformation.actions.delete;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.talend.dataprep.api.action.ActionDefinition;
+import org.talend.dataprep.api.dataset.ColumnMetadata;
+import org.talend.dataprep.api.dataset.RowMetadata;
+import org.talend.dataprep.api.dataset.row.DataSetRow;
+import org.talend.dataprep.api.dataset.statistics.DataFrequency;
+import org.talend.dataprep.api.dataset.statistics.Statistics;
+import org.talend.dataprep.api.type.Type;
+import org.talend.dataprep.transformation.actions.AbstractMetadataBaseTest;
+import org.talend.dataprep.transformation.actions.ActionMetadataTestUtils;
+import org.talend.dataprep.transformation.actions.category.ActionCategory;
+import org.talend.dataprep.transformation.api.action.ActionTestWorkbench;
+
+import java.io.IOException;
+import java.util.*;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.talend.dataprep.api.dataset.ColumnMetadata.Builder.column;
+import static org.talend.dataprep.transformation.actions.ActionMetadataTestUtils.getColumn;
+
+public class DeleteAllEmptyColumnsTest extends AbstractMetadataBaseTest<DeleteAllEmptyColumns> {
+
+    /** the action to test. */
+    private DeleteAllEmptyColumns action = new DeleteAllEmptyColumns();
+
+    private Map<String, String> parameters;
+
+    private RowMetadata rowMetadata;
+
+    public DeleteAllEmptyColumnsTest() {
+        super(new DeleteAllEmptyColumns());
+    }
+
+    @Override
+    protected CreateNewColumnPolicy getCreateNewColumnPolicy() {
+        return CreateNewColumnPolicy.NA;
+    }
+
+    @Before
+    public void init() throws IOException {
+        List<ColumnMetadata> columns = new ArrayList<>();
+        Statistics statistics1 = new Statistics();
+        statistics1.setDataFrequencies(Arrays.asList(new DataFrequency("blip", 10)));
+        ColumnMetadata columnMetadata = ColumnMetadata.Builder.column() //
+                .type(Type.INTEGER) //
+                .computedId("0000") //
+                .valid(10) //
+                .statistics(statistics1) //
+                .build();
+        columns.add(columnMetadata);
+        Statistics statistics2 = new Statistics();
+        statistics2.setDataFrequencies(Arrays.asList(new DataFrequency("", 10)));
+        columnMetadata = ColumnMetadata.Builder.column() //
+                .type(Type.STRING) //
+                .computedId("0001") //
+                .valid(0) //
+                .empty(10) //
+                .invalid(0) //
+                .statistics(statistics2) //
+                .build();
+        columns.add(columnMetadata);
+        Statistics statistics3 = new Statistics();
+        statistics3.setDataFrequencies(Arrays.asList(new DataFrequency("blop", 10)));
+        columnMetadata = ColumnMetadata.Builder.column() //
+                .type(Type.STRING) //
+                .statistics(statistics3) //
+                .computedId("0002") //
+                .valid(10) //
+                .build();
+        columns.add(columnMetadata);
+        Statistics statistics4 = new Statistics();
+        statistics4.setDataFrequencies(Arrays.asList(new DataFrequency("", 4), new DataFrequency(" ", 6)));
+        columnMetadata = ColumnMetadata.Builder.column() //
+            .type(Type.STRING) //
+            .computedId("0003") //
+            .empty(10) //
+            .statistics(statistics4)
+            .build();
+        columns.add(columnMetadata);
+        rowMetadata = new RowMetadata();
+        rowMetadata.setColumns(columns);
+
+        parameters = ActionMetadataTestUtils
+                .parseParameters(DeleteAllEmptyColumns.class.getResourceAsStream("deleteAllEmptyColumnsAction.json"));
+    }
+
+    @Test
+    public void testAdapt() throws Exception {
+        assertThat(action.adapt((ColumnMetadata) null), is(action));
+        ColumnMetadata column = column().name("myColumn").id(0).type(Type.STRING).build();
+        assertThat(action.adapt(column), is(action));
+    }
+
+    @Test
+    public void testCategory() throws Exception {
+        assertThat(action.getCategory(Locale.US), is(ActionCategory.DATA_CLEANSING.getDisplayName(Locale.US)));
+    }
+
+    @Test
+    public void should_accept_column() {
+        assertTrue(action.acceptField(getColumn(Type.STRING)));
+        assertTrue(action.acceptField(getColumn(Type.NUMERIC)));
+        assertTrue(action.acceptField(getColumn(Type.FLOAT)));
+        assertTrue(action.acceptField(getColumn(Type.DATE)));
+        assertTrue(action.acceptField(getColumn(Type.BOOLEAN)));
+        assertTrue(action.acceptField(getColumn(Type.ANY)));
+    }
+
+    @Test
+    public void should_delete_column() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0001", "1");
+        values.put("0002", "");
+        values.put("0003", "blop");
+        values.put("0004", " ");
+        final DataSetRow row = new DataSetRow(rowMetadata, values);
+        parameters.put(DeleteAllEmptyColumns.ACTION_PARAMETER, DeleteAllEmptyColumns.DELETE);
+
+        // when
+        ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+
+        // then
+        final int rowSize = row.getRowMetadata().getColumns().size();
+        assertEquals(2, rowSize);
+    }
+
+    @Test
+    public void should_delete_only_one_column() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0001", "1");
+        values.put("0002", "");
+        values.put("0003", "blop");
+        values.put("0004", " ");
+        final DataSetRow row = new DataSetRow(rowMetadata, values);
+        parameters.put(DeleteAllEmptyColumns.ACTION_PARAMETER, DeleteAllEmptyColumns.KEEP);
+
+        // when
+        ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+
+        // then
+        final int rowSize = row.getRowMetadata().getColumns().size();
+        assertEquals(3, rowSize);
+    }
+
+    @Test
+    public void should_have_expected_behavior() {
+        assertEquals(1, action.getBehavior().size());
+        assertTrue(action.getBehavior().contains(ActionDefinition.Behavior.METADATA_DELETE_COLUMNS));
+    }
+
+}

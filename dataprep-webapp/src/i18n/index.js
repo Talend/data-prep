dd9c95bf1c1f14129b0e1bed0e085261c3667b98
Fabien Rassinier <frassinier@talend.com>
2018-04-23 14:19:31 +0200

    fix(TDP-5567): style

--- dataprep-webapp/src/i18n/index.js
+++ dataprep-webapp/src/i18n/index.js
@@ -1,9 +1,5 @@
-import TRANSLATIONS_EN from './en';
-import TRANSLATIONS_FR from './fr';
-import TRANSLATIONS_JA from './ja';
+import en from './en';
+import fr from './fr';
+import ja from './ja';
 
-export default {
-	en: TRANSLATIONS_EN,
-	fr: TRANSLATIONS_FR,
-	ja: TRANSLATIONS_JA,
-};
+export default { en, fr, ja };

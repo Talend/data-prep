e589761771c79177084fd0e3b699f95dbc0fed50
Fabien Rassinier <frassinier@talend.com>
2018-05-30 11:02:52 +0200

    i18n and webpack

--- dataprep-webapp/src/app/next/components/App.container.js
+++ dataprep-webapp/src/app/next/components/App.container.js
@@ -5,14 +5,13 @@ import { Notification, ShortcutManager } from '@talend/react-containers';
 import AppLoader from '@talend/react-containers/lib/AppLoader/index';
 
 import { I18nextProvider } from 'react-i18next';
-import i18n from '@talend/data-quality-semantic-ee/lib/app/i18n';
 
 import AboutModal from './AboutModal';
 import PreparationCreatorModal from '../../components/preparation-creator/index';
 
 export default function App(props) {
 	return (
-		<I18nextProvider i18n={i18n}>
+		<I18nextProvider>
 			<AppLoader>
 				<IconsProvider />
 				<ShortcutManager view="shortcuts" />

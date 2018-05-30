74eb856c011bc5f01cf7a99578d7c93e7acced1e
Nicolas Comont <ncomont@talend.com>
2018-05-30 13:24:11 +0200

    relative urls

--- dataprep-webapp/src/app/next/constants/search.js
+++ dataprep-webapp/src/app/next/constants/search.js
@@ -1,5 +1,5 @@
 export const DOCUMENTATION_SEARCH_URL = 'https://www.talendforge.org/find/api/THC.php';
-export const DATAPREP_SEARCH_URL = 'http://localhost:3000/api/search?path=/&name=';
+export const DATAPREP_SEARCH_URL = '/api/search?path=/&name=';
 export const DEBOUNCE_TIMEOUT = 500;
 export const SEARCH_CATEGORIES_BY_PROVIDER = {
 	tdp: ['dataset', 'preparation', 'folder'],

df785ce403625afab7676542050ce61726d39077
Fabien Rassinier <frassinier@talend.com>
2018-06-01 13:54:39 +0200

    take into account location pathname params

--- dataprep-webapp/src/app/next/sagas/preparation.saga.js
+++ dataprep-webapp/src/app/next/sagas/preparation.saga.js
@@ -1,5 +1,6 @@
 import { call, take, put, select } from 'redux-saga/effects';
 import http from '@talend/react-cmf/lib/sagas/http';
+import matchPath from '@talend/react-cmf/lib/sagaRouter/matchPath';
 import api, { actions } from '@talend/react-cmf';
 import {
 	CANCEL_RENAME_PREPARATION,
@@ -45,12 +46,16 @@ function* duplicate() {
 		yield call(fetch);
 	}
 }
+}
 
 function* fetch() {
 	while (true) {
 		const { payload } = yield take(FETCH_PREPARATIONS);
+		const path = window.location.pathname;
+		const match = matchPath(path, { path: '/preparations/:folderId' });
+		const folderId = (match && match.params.folderId) || payload.folderId;
 		yield put(
-			actions.http.get(`http://localhost:8888/api/folders/${payload.folderId}/preparations`, {
+			actions.http.get(`http://localhost:8888/api/folders/${folderId}/preparations`, {
 				cmf: {
 					collectionId: 'preparations',
 				},

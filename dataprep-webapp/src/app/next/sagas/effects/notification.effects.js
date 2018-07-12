4debaa5d05dbcbadd26e9da3ad37d2d5b56ca862
charlesnguyen <cnguyen@talend.com>
2018-07-12 12:11:02 +0200

    TDP-5832: add unit tests

--- dataprep-webapp/src/app/next/sagas/effects/notification.effects.js
+++ dataprep-webapp/src/app/next/sagas/effects/notification.effects.js
@@ -2,12 +2,13 @@ import objectId from 'bson-objectid';
 import { put, select } from 'redux-saga/effects';
 import { actions } from '@talend/react-cmf';
 
-const COMPONENT_NAME = 'Container(Notification)';
-const COMPONENT_KEY = 'Notification';
+export const COMPONENT_NAME = 'Container(Notification)';
+export const COMPONENT_KEY = 'Notification';
 
 export function* push(notification) {
 	const path = [COMPONENT_NAME, COMPONENT_KEY, 'notifications'];
 	let notifications = yield select(state => state.cmf.components.getIn(path));
+	console.log(notifications);
 	notifications = notifications.push(notification);
 	yield put(actions.components.mergeState(COMPONENT_NAME, COMPONENT_KEY, { notifications }));
 }

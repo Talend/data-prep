da3af46c408b3c9f3c95d78ec976e26f3ac070ef
Nicolas Comont <ncomont@talend.com>
2018-07-13 15:16:00 +0200

    fix: add test

---
+++ dataprep-webapp/src/app/services/confirm/confirm-service.spec.js
@@ -0,0 +1,37 @@
+/*  ============================================================================
+
+ Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+
+ This source code is available under agreement available at
+ https://github.com/Talend/data-prep/blob/master/LICENSE
+
+ You should have received a copy of the agreement
+ along with this program; if not, write to Talend SA
+ 9 rue Pages 92150 Suresnes, France
+
+ ============================================================================*/
+
+fdescribe('confirm service', () => {
+	beforeEach(angular.mock.module('data-prep.services.confirm'));
+
+	beforeEach(angular.mock.module('pascalprecht.translate', ($translateProvider) => {
+		$translateProvider.translations('en', {
+			a: 'translation_a',
+			b: 'translation_b',
+			c: 'translation_c',
+		});
+		$translateProvider.preferredLanguage('en');
+	}));
+
+	beforeEach(inject((StateService) => {
+		spyOn(StateService, 'showConfirmationModal').and.returnValue();
+		spyOn(StateService, 'hideConfirmationModal').and.returnValue();
+	}));
+
+	it('should prompt', inject(($rootScope, StateService, ConfirmService) => {
+		ConfirmService.confirm();
+		$rootScope.$digest();
+		$rootScope.$digest();
+		expect(StateService.showConfirmationModal).toHaveBeenCalled();
+	}));
+});

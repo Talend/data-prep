31ad5ebf369783cc97e2440ea7c5bb2e8a322682
fabballe <fabballe@talend.com>
2018-07-26 14:56:53 +0200

    chore(TDP-5734): Allow maintenance task to execute in a single tenant context

--- dataprep-maintenance/src/main/java/org/talend/dataprep/maintenance/executor/MaintenanceScheduler.java
+++ dataprep-maintenance/src/main/java/org/talend/dataprep/maintenance/executor/MaintenanceScheduler.java
@@ -6,29 +6,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.talend.dataprep.security.Security;
 import org.talend.tenancy.ForAll;
 
 import javax.annotation.PostConstruct;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.talend.dataprep.maintenance.executor.ScheduleFrequency.REPEAT;
 
 @Component
-public class TaskScheduler {
+public class MaintenanceScheduler {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TaskScheduler.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(MaintenanceScheduler.class);
 
     private static final ScheduleFrequency DEFAULT_SCHEDULE_FREQUENCY = REPEAT;
 
     private static final String EVERY_DAY_CRON_EXPRESSION = "0 0 3 * * *";
-
 
     @Autowired
     private List<MaintenanceTaskProcess> maintenanceTasks;
 
     @Autowired
     private ForAll forAll;
+
+    @Autowired
+    private Security security;
+
+    private Map<String, Long> runningTask = new HashMap<>();
 
     @PostConstruct
     public void launchOnceTask() {
@@ -48,16 +55,37 @@ public class TaskScheduler {
     private void runMaintenanceTask(ScheduleFrequency frequency) {
         LOGGER.info("Starting scheduled task with frequency {}", frequency);
         forAll.execute(() -> true, () -> {
+
+            String tenantId = security.getTenantId();
             // filter maintenance task marked as with this frequency and run it
             getTaskForFrequency(frequency)
                     .forEach(
                             task -> {
-                                LOGGER.debug("Scheduler task {} process started @ {}.", task.getClass(), System.currentTimeMillis());
-                                task.execute();
-                                LOGGER.debug("Scheduler task {} process ended @ {}.", task.getClass(), System.currentTimeMillis());
+                                String taskKey = task.getClass() + "_" + tenantId;
+                                if (!isAlreadyRunning(taskKey)) {
+                                    executeTask(tenantId, task, taskKey);
+                                } else {
+                                    LOGGER.warn("Scheduled task {} for tenant {} is already running", task.getClass(), tenantId);
+                                }
                             }
                     );
         });
+    }
+
+    private void executeTask(String tenantId, MaintenanceTaskProcess task, String taskKey) {
+        Long startedTime = System.currentTimeMillis();
+        try {
+            LOGGER.debug("Scheduled task {} process for tenant {} started @ {}.", task.getClass(), tenantId, startedTime);
+            runningTask.put(taskKey, startedTime);
+            task.execute();
+            LOGGER.debug("Scheduled task {} process for tenant {} ended @ {}.", task.getClass(), tenantId, System.currentTimeMillis());
+        } finally {
+            runningTask.remove(taskKey);
+        }
+    }
+
+    protected boolean isAlreadyRunning(String taskKey) {
+        return runningTask.containsKey(taskKey);
     }
 
     private Stream<MaintenanceTaskProcess> getTaskForFrequency(ScheduleFrequency frequency) {
--- dataprep-maintenance/src/test/java/org/talend/dataprep/maintenance/executor/MaintenanceSchedulerTest.java
+++ dataprep-maintenance/src/test/java/org/talend/dataprep/maintenance/executor/MaintenanceSchedulerTest.java
@@ -3,24 +3,32 @@ package org.talend.dataprep.maintenance.executor;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Spy;
 import org.talend.dataprep.maintenance.BaseMaintenanceTest;
+import org.talend.dataprep.security.Security;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-public class TaskSchedulerTest extends BaseMaintenanceTest {
+public class MaintenanceSchedulerTest extends BaseMaintenanceTest {
 
     @InjectMocks
-    private TaskScheduler scheduler;
+    @Spy
+    private MaintenanceScheduler scheduler;
 
     @Spy
     private List<MaintenanceTaskProcess> listTask = new ArrayList<>();
+
+    @Mock
+    private Security security;
 
     @Before
     public void init() {
@@ -38,6 +46,28 @@ public class TaskSchedulerTest extends BaseMaintenanceTest {
             boolean condition = (i % 2 == 0);
             when(listTask.get(i).condition()).thenReturn(() -> condition);
         }
+
+        when(security.getTenantId()).thenReturn("TENANT_ID");
+    }
+
+    @Test
+    public void testLaunchAlreadyRunningTask() {
+
+        when(scheduler.isAlreadyRunning(any(String.class))).thenReturn(false);
+        scheduler.launchOnceTask();
+
+        // task are not running. We should call execute
+        verify(listTask.get(0), times(1)).execute();
+
+        // we reset the number of called method on the mock
+        reset(listTask.get(0));
+
+        when(scheduler.isAlreadyRunning(any(String.class))).thenReturn(true);
+        scheduler.launchOnceTask();
+
+        // task are running. We should call execute
+        verify(listTask.get(0), times(0)).execute();
+
     }
 
     @Test
@@ -64,7 +94,7 @@ public class TaskSchedulerTest extends BaseMaintenanceTest {
 
         scheduler.launchNightlyTask();
 
-        // only task with ScheduleFrequency = ONCE are call
+        // only task with ScheduleFrequency = NIGHT are call
         verify(listTask.get(2), times(1)).performTask();
         verify(listTask.get(3), times(0)).performTask();
         verify(listTask.get(6), times(1)).performTask();
@@ -84,7 +114,7 @@ public class TaskSchedulerTest extends BaseMaintenanceTest {
 
         scheduler.launchRepeatlyTask();
 
-        // only task with ScheduleFrequency = ONCE are call
+        // only task with ScheduleFrequency = REPEAT are call
         verify(listTask.get(4), times(1)).performTask();
         verify(listTask.get(5), times(0)).performTask();
 

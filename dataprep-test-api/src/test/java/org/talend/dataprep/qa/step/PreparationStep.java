fe1efc92de09cc36d84388c95eb81f60f3ae60d3
Emeline Logeart <elogeart@talend.com>
2018-08-02 11:35:11 +0200

    * fix merge

--- dataprep-test-api/src/test/java/org/talend/dataprep/qa/step/PreparationStep.java
+++ dataprep-test-api/src/test/java/org/talend/dataprep/qa/step/PreparationStep.java
@@ -199,7 +199,7 @@ public class PreparationStep extends DataPrepStep {
     public void thePreparationShouldContainTheFollowingColumns(String preparationName, List<String> columns)
             throws Exception {
         Response response =
-                api.getPreparationContent(context.getPreparationId(suffixName(preparationName)), "head", "HEAD");
+                api.getPreparationContent(context.getPreparationId(suffixName(preparationName)), "head", "HEAD", "");
         response.then().statusCode(OK.value());
 
         checkColumnNames(preparationName, columns, response.jsonPath().getList("metadata.columns.name", String.class));

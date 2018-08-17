f514496089bd7fbf4982a31857805c5279aa250f
Fabien Rassinier <frassinier@talend.com>
2018-08-17 11:45:42 +0200

    fix(TDP-6007): DQ translations

--- dataprep-webapp/src/app/i18n.js
+++ dataprep-webapp/src/app/i18n.js
@@ -1,15 +1,10 @@
-import i18next from 'i18next'; // eslint-disable-line import/no-named-as-default-member
+import i18next, { init } from 'i18next'; // eslint-disable-line import/no-named-as-default-member
 
 import constants from './next/constants';
 
 import { default as locales } from './next/locales';
 
 const I18N = constants.I18N;
-
-const NAMESPACES = [
-	I18N.DATASET_APP_NAME_SPACE,
-	I18N.TUI_COMPONENTS_NAME_SPACE,
-];
 
 function setNameSpace(locale, namespace) {
 	// overwrite all existing resources if exists
@@ -20,15 +15,27 @@ function setNameSpace(locale, namespace) {
 	);
 }
 
-// eslint-disable-next-line import/no-named-as-default-member
-i18next.init({
-	fallbackLng: 'en',
+export const fallbackLng = I18N.EN_LOCALE;
+export const defaultNS = I18N.TUI_COMPONENTS_NAMESPACE;
+export const fallbackNS = I18N.TUI_COMPONENTS_NAMESPACE;
+
+const i18n = init({
+	fallbackLng,
 	debug: false,
 	wait: true, // globally set to wait for loaded translations in translate hoc
-	lng: I18N.EN_LOCALE,
 	interpolation: { escapeValue: false },
+	ns: [
+		I18N.DATASET_APP_NAMESPACE,
+		I18N.TUI_COMPONENTS_NAMESPACE,
+		I18N.TUI_FORMS_NAMESPACE,
+	],
+	defaultNS,
+	fallbackNS,
 });
 
-I18N.LOCALES.forEach(locale => NAMESPACES.forEach(namespace => setNameSpace(locale, namespace)));
+I18N.LOCALES.forEach(locale => [
+	I18N.DATASET_APP_NAMESPACE,
+	I18N.TUI_COMPONENTS_NAMESPACE,
+].forEach(namespace => setNameSpace(locale, namespace)));
 
-export default i18next;
+export default i18n;

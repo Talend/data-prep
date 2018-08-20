ef5fc82635fd6cb3b67a11f7bbccb41a7dc8b54a
charlesnguyen <cnguyen@talend.com>
2018-08-20 10:46:01 +0200

    fix(TDP-5280): CMF - Browser back button management - review

--- dataprep-webapp/src/app/next/sagas/effects/bootstrap.effects.js
+++ dataprep-webapp/src/app/next/sagas/effects/bootstrap.effects.js
@@ -8,10 +8,17 @@ import { refresh } from './preparation.effects';
  * @returns {IterableIterator<*>}
  */
 export function* fetch() {
+	yield* fetchSettings();
+	// this should be called here because refresh use settings in the store
+	yield* initializeRouter();
+}
+
+function* fetchSettings() {
 	const data = yield store.get('settings');
 	yield put(actions.collections.addOrReplace('settings', data));
+}
 
-	// this should be called here because refresh use settings in the store
+function* initializeRouter() {
 	const routes = {
 		'/preparations': { saga: refresh, runOnExactMatch: true },
 		'/preparations/:folderId': { saga: refresh, runOnExactMatch: true },

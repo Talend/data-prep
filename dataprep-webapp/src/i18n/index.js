d3617d1b4938e8eb8933582adf6d70c4f132da60
Fabien Rassinier <frassinier@talend.com>
2018-09-11 13:55:23 +0200

    feat(TDP-4900): add chinese translations

--- dataprep-webapp/src/i18n/index.js
+++ dataprep-webapp/src/i18n/index.js
@@ -3,4 +3,20 @@ import fr from './fr';
 import ja from './ja';
 import zh from './zh';
 
+import d3LocaleEn from '../lib/d3/locale.en';
+import d3LocaleFr from '../lib/d3/locale.fr';
+import d3LocaleJa from '../lib/d3/locale.ja';
+import d3LocaleZh from '../lib/d3/locale.zh';
+
+const d3CustomLocales = {
+	en: d3LocaleEn,
+	fr: d3LocaleFr,
+	ja: d3LocaleJa,
+	zh: d3LocaleZh,
+};
+
+export function getD3Locale(locale) {
+	return d3CustomLocales[locale];
+}
+
 export default { en, fr, ja, zh };

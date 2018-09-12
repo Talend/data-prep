fbc90f28f532fefdf7cab2cf96a788a1a42ce3ce
Fabien Rassinier <frassinier@talend.com>
2018-09-12 18:12:05 +0200

    fix

--- dataprep-webapp/src/app/components/dataset/import/dataset-import.controller.js
+++ dataprep-webapp/src/app/components/dataset/import/dataset-import.controller.js
@@ -177,7 +177,7 @@ export default class DatasetImportCtrl {
 	 * @param changes Form changes
 	 */
 	onDatastoreFormSubmit(event, changes) {
-		const { formData, definitionName = (this.locationType || LIVE_LOCATION_TYPE) } = changes;
+		const { formData, definitionName = (this.locationType || (this.item && this.item.componentType) || LIVE_LOCATION_TYPE) } = changes;
 		if (this.submitLock) {
 			const formsData = {
 				dataStoreProperties: formData,

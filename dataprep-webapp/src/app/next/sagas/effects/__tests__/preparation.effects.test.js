33c41e7d6ca5051143edad439b22cef850d2bce4
Nicolas Comont <ncomont@talend.com>
2018-09-24 14:52:28 +0200

    i18n

--- dataprep-webapp/src/app/next/sagas/effects/__tests__/preparation.effects.test.js
+++ dataprep-webapp/src/app/next/sagas/effects/__tests__/preparation.effects.test.js
@@ -334,62 +334,6 @@ describe('preparation', () => {
 		});
 	});
 
-	describe('remove', () => {
-		it('should open remove folder modal', () => {
-			const payload = { id: 'folderId', name: 'folderName' };
-			const gen = effects.openRemoveFolderModal(payload);
-			const effect = gen.next(payload).value.PUT.action;
-			expect(effect.type).toEqual('REACT_CMF.COMPONENT_MERGE_STATE');
-			expect(effect.key).toEqual('ConfirmDialog');
-			expect(effect.componentName).toEqual('CMFContainer(ConfirmDialog)');
-			expect(effect.componentState).toEqual(new Map({
-				header: 'Remove a folder',
-				show: true,
-				children: 'REMOVE_FOLDER_MODAL_CONTENT',
-				validateAction: 'folder:remove',
-				cancelAction: 'folder:remove:close',
-				folderId: 'folderId',
-				folderName: 'folderName',
-			}));
-
-			expect(gen.next().done).toBeTruthy();
-		});
-
-		it('should close remove folder modal', () => {
-			const gen = effects.closeRemoveFolderModal();
-			const effect = gen.next().value.PUT.action;
-			expect(effect.type).toEqual('REACT_CMF.COMPONENT_MERGE_STATE');
-			expect(effect.key).toEqual('ConfirmDialog');
-			expect(effect.componentName).toEqual('CMFContainer(ConfirmDialog)');
-			expect(effect.componentState).toEqual(new Map({ show: false }));
-
-			expect(gen.next().done).toBeTruthy();
-		});
-
-		it('should remove folder', () => {
-			const gen = effects.removeFolder();
-			expect(gen.next().value.SELECT).toBeDefined();
-			expect(gen.next(IMMUTABLE_SETTINGS).value.SELECT).toBeDefined();
-			const effect = gen.next('folderId').value.CALL;
-			expect(effect.fn).toEqual(http.delete);
-			expect(effect.args[0]).toEqual('/api/folders/folderId');
-			expect(gen.next(API_RESPONSE).value).toEqual(
-				call(effects.refreshCurrentFolder),
-			);
-			expect(gen.next().value.SELECT).toBeDefined();
-			expect(gen.next().value.PUT.action.type).toBe('TDP_SUCCESS_NOTIFICATION');
-			expect(gen.next().value).toEqual(
-				call(effects.closeRemoveFolderModal),
-			);
-			expect(gen.next().value).toEqual(
-				call(delay, 500),
-			);
-			expect(gen.next().value).toEqual(
-				call(preparationWatcher['preparation:folder:remove']),
-			);
-		});
-	});
-
 	describe('add', () => {
 		it('should open add folder modal', () => {
 			const gen = effects.openAddFolderModal();

04e7984b7b6d896125ca1dac22d5035a78196349
Henri-Mayeul de Benque <hmdebenque@talend.com>
2018-10-25 18:02:05 +0200

    fix(TDP-6233): fix concurrency problem

--- dataprep-transformation/src/test/java/org/talend/dataprep/transformation/ReactiveStepMetadataRepositoryTest.java
+++ dataprep-transformation/src/test/java/org/talend/dataprep/transformation/ReactiveStepMetadataRepositoryTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -24,8 +25,6 @@ import org.talend.dataprep.transformation.service.StepMetadataRepository;
 @RunWith(MockitoJUnitRunner.class)
 public class ReactiveStepMetadataRepositoryTest {
 
-    private static final String TASK_COMPLETED_SUCCESSFULLY = "OK";
-
     @InjectMocks
     private ReactiveStepMetadataRepository reactiveStepMetadataRepository;
 
@@ -36,55 +35,63 @@ public class ReactiveStepMetadataRepositoryTest {
     private SecurityProxy proxy;
 
     @Test
-    public void testEmitUpdateMessage() throws InterruptedException, ExecutionException {
+    public void testEmitUpdateMessage() throws InterruptedException {
         // given
+        CountDownLatch delegateInvalidateLatch = new CountDownLatch(1);
         RowMetadata rowMetadata = new RowMetadata();
         String stepId = "10";
-        final CompletableFuture<String> future = new CompletableFuture<>();
         Mockito.doAnswer(invocation -> {
-            future.complete(TASK_COMPLETED_SUCCESSFULLY);
+            delegateInvalidateLatch.countDown();
             return null;
         }).when(delegate).update(stepId, rowMetadata);
+
+        CountDownLatch proxyReleaseLatch = new CountDownLatch(1);
+        Mockito.doAnswer(invocation -> {
+            proxyReleaseLatch.countDown();
+            return null;
+        }).when(proxy).releaseIdentity();
 
         // when
         reactiveStepMetadataRepository.update(stepId, rowMetadata);
 
         // then
-        String result = null;
-        try {
-            result = future.get(1, TimeUnit.SECONDS);
-        } catch (TimeoutException e) {
+        boolean awaitSuccess = delegateInvalidateLatch.await(1, TimeUnit.SECONDS);
+        if (!awaitSuccess) {
             fail("Delegate was never called");
         }
-        assertEquals(TASK_COMPLETED_SUCCESSFULLY, result); // should always be true or else would have failed before
-        verify(proxy, times(1)).asTechnicalUser();
-        verify(delegate, times(1)).update(stepId, rowMetadata);
-        verify(proxy, times(1)).releaseIdentity();
+        verify(proxy).asTechnicalUser();
+        verify(delegate).update(stepId, rowMetadata); // not really needed as the future verify that
+        proxyReleaseLatch.await(1, TimeUnit.SECONDS); // To be sure the method has been called
+        verify(proxy).releaseIdentity();
     }
 
     @Test
-    public void testEmitInvalidateMessage() throws InterruptedException, ExecutionException {
+    public void testEmitInvalidateMessage() throws InterruptedException {
         // given
-        final CompletableFuture<String> future = new CompletableFuture<>();
+        CountDownLatch delegateInvalidateLatch = new CountDownLatch(1);
         String stepId = "11";
         Mockito.doAnswer(invocation -> {
-            future.complete(TASK_COMPLETED_SUCCESSFULLY);
+            delegateInvalidateLatch.countDown();
             return null;
         }).when(delegate).invalidate(stepId);
+
+        CountDownLatch proxyReleaseLatch = new CountDownLatch(1);
+        Mockito.doAnswer(invocation -> {
+            proxyReleaseLatch.countDown();
+            return null;
+        }).when(proxy).releaseIdentity();
 
         // when
         reactiveStepMetadataRepository.invalidate(stepId);
 
         // then
-        String result = null;
-        try {
-            result = future.get(1, TimeUnit.SECONDS);
-        } catch (TimeoutException e) {
+        boolean awaitSuccess = delegateInvalidateLatch.await(1, TimeUnit.SECONDS);
+        if (!awaitSuccess) {
             fail("Delegate was never called");
         }
-        assertEquals(TASK_COMPLETED_SUCCESSFULLY, result); // should always be true or else would have failed before
-        verify(proxy, times(1)).asTechnicalUser();
-        verify(delegate, times(1)).invalidate(stepId);
-        verify(proxy, times(1)).releaseIdentity();
+        verify(proxy).asTechnicalUser();
+        verify(delegate).invalidate(stepId); // not really needed as the future verify that
+        proxyReleaseLatch.await(1, TimeUnit.SECONDS); // To be sure the method has been called
+        verify(proxy).releaseIdentity();
     }
 }

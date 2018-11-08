5a849a83efec5e2457679292be9eee6425ffd6b4
Fabien Rassinier <frassinier@talend.com>
2018-11-08 10:39:13 +0100

    feat(TPD-5909): add unit test when http error

--- dataprep-webapp/src/app/next/sagas/effects/__tests__/preparation.effects.test.js
+++ dataprep-webapp/src/app/next/sagas/effects/__tests__/preparation.effects.test.js
@@ -101,6 +101,34 @@ describe('preparation', () => {
 
 			expect(gen.next().done).toBeTruthy();
 		});
+
+		it('should not trying to open preparation if error', () => {
+			const error = new HTTPError({
+				data: { message: 'err message' },
+				response: { statusText: 'err' },
+			});
+			const CUSTOM_FOLDER_ID = 'FAKE_CUSTOM_FOLDER_ID';
+			const payload = {
+				id: 'DATASET_ID',
+				folderId: CUSTOM_FOLDER_ID,
+			};
+			const gen = effects.create(payload);
+
+			const effectPUT = gen.next().value.PUT.action;
+			expect(effectPUT.type).toBe('REACT_CMF.COLLECTION_ADD_OR_REPLACE');
+			expect(effectPUT.collectionId).toBe('currentFolderId');
+			expect(effectPUT.data).toBe(CUSTOM_FOLDER_ID);
+
+			expect(gen.next().value.SELECT).toBeDefined();
+
+			const effectCALL = gen.next(IMMUTABLE_SETTINGS).value.CALL;
+			expect(effectCALL.fn).toEqual(http.post);
+			expect(effectCALL.args[0]).toEqual(
+				`/api/preparations?folder=${CUSTOM_FOLDER_ID}`,
+			);
+
+			expect(gen.next(error).done).toBeTruthy();
+		});
 	});
 
 	describe('fetch', () => {

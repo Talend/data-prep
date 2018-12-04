150326d49385e44c27e157e64e23cd1b1a06424f
Nicolas Comont <ncomont@talend.com>
2018-12-04 14:29:44 +0100

    fix: rename

--- dataprep-webapp/src/app/next/services/preparation.service.js
+++ dataprep-webapp/src/app/next/services/preparation.service.js
@@ -1,56 +1,3 @@
-import i18n from '../../i18n';
-
-function getHomeFolderLabel() {
-	return i18n.t('tdp-app:FOLDER_HOME');
-}
-
-function transform({ folders = [], preparations = [] }) {
-	const adaptedFolders = folders.map(folder => ({
-		author: folder.ownerId,
-		className: 'list-item-folder',
-		icon: 'talend-folder',
-		id: folder.id,
-		name: folder.name,
-		type: 'folder',
-	}));
-	const adaptedPreparations = preparations.map(prep => ({
-		author: prep.author,
-		className: 'list-item-preparation',
-		datasetName: prep.dataset.dataSetName,
-		icon: 'talend-dataprep',
-		id: prep.id,
-		name: prep.name,
-		nbSteps: prep.steps.length - 1,
-		type: 'preparation',
-	}));
-	return adaptedFolders.concat(adaptedPreparations);
-}
-
-function transformTree(input) {
-	const t = (item) => {
-		return {
-			id: item.folder.id,
-			name: item.folder.name || getHomeFolderLabel(),
-			children: item.children.map(t),
-		};
-	};
-
-	return [t(input)];
-}
-
-function transformFolder({ folder, hierarchy }) {
-	return [
-		...hierarchy,
-		folder,
-	].map(folder =>
-		({
-			id: folder.id,
-			text: folder.name || getHomeFolderLabel(),
-			title: folder.name || getHomeFolderLabel(),
-			actionCreator: 'folder:open',
-		}));
-}
-
 function _adapt(value) {
 	return (value || '').toString();
 }
@@ -67,8 +14,5 @@ function sort(by, asc) {
 }
 
 export default {
-	transform,
-	transformTree,
-	transformFolder,
 	sort,
 };
